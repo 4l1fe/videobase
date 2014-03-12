39433764f4a7dd9f8e2f655bad6e0b99fd751e87
tumani1 <tumani1@yandex.ru>
2014-03-12 17:35:52 +0400

    - перенес приложение контент в папку apps - поправил файл моделей на счет импортов других моделей - поправил поля с foreign key

---
+++ apps/content/models.py
@@ -0,0 +1,62 @@
+# coding: utf-8
+
+from django.db import models
+
+from apps.users.models import Users
+from apps.films.models import Films, Seasons
+
+
+#############################################################################################################
+#
+class Content(models.Model):
+    film                 = models.ForeignKey(Films, verbose_name=u'Фильм')
+    name                 = models.CharField(max_length=255, verbose_name=u'Название')
+    name_orig            = models.CharField(max_length=255, verbose_name =u'Оригинальное название')
+    sNumber              = models.IntegerField(verbose_name=u'Номер сезона', null = True, blank =True)
+    description          = models.TextField(verbose_name=u'Описание')
+    release_date         = models.DateTimeField(verbose_name=u'Дата выхода')
+    season               = models.ForeignKey(Seasons, null=True, blank=True, verbose_name=u'Сезоны')
+    viewer_cnt           = models.IntegerField(verbose_name=u'Количество посмотревших за все время')
+    viewer_lastweek_cnt  = models.IntegerField(verbose_name=u'Количество посмотревших за последнюю неделю')
+    viewer_lastmonth_cnt = models.IntegerField(verbose_name=u'Количество посмотревших за последний месяц')
+
+    class Meta:
+        # Имя таблицы в БД
+        db_table = 'content'
+        verbose_name = u'Место'
+        verbose_name_plural = u'Места'
+
+
+#############################################################################################################
+#
+class Locations(models.Model):
+    content    = models.ForeignKey(Content, verbose_name=u'Контент')
+    lType      = models.CharField(max_length=255, verbose_name=u'Тип')
+    quality    = models.CharField(max_length=40, verbose_name=u'Качество')
+    subtitles  = models.CharField(max_length=40, verbose_name=u'Субтитры')
+    price      = models.FloatField(verbose_name=u'Цена')
+    price_type = models.CharField(max_length=40, verbose_name=u'Тип цены')
+    value      = models.CharField(max_length=40, verbose_name=u'Ценность')
+    
+    class Meta:
+        # Имя таблицы в БД
+        db_table = 'locations'
+        verbose_name = u'Место'
+        verbose_name_plural = u'Места'
+
+
+#############################################################################################################
+#
+class Comments(models.Model):
+    user       = models.ForeignKey(Users, verbose_name=u'Пользователь')
+    content    = models.IntegerField(verbose_name=u'Контент')
+    cText      = models.TextField(verbose_name=u'Tекст комментария')
+    parent_id  = models.IntegerField(verbose_name=u'Родительский комментарий')
+    cStatus    = models.CharField(max_length=40, verbose_name=u'Статус')
+    created    = models.DateTimeField(auto_now_add=True, verbose_name=u'Создан')
+    
+    class Meta:
+        # Имя таблицы в БД
+        db_table = 'comments'
+        verbose_name = u'Комментарий'
+        verbose_name_plural = u'Комментарии'
