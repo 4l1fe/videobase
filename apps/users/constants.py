27b0f0014bdf7ddcf763bf13205a4454e090401f
Maxim Pechnikov <mpechnikov@aaysm.com>
2014-03-12 18:13:22 +0400

    fixed choose user pic

--- apps/users/constants.py
+++ apps/users/constants.py
@@ -4,8 +4,18 @@ import os
 from videobase.settings  import STATIC_PATH
 
 # Путь к каталогу с фото
-APP_PHOTO_DIR =  os.path.join(STATIC_PATH, 'photo')
+PERSON_PHOTO_DIR =  os.path.join(STATIC_PATH, 'upload', 'people', 'photos')
+USER_PIC_DIR =  os.path.join(STATIC_PATH, 'upload', 'users', 'user_pic')
 REL_TYPES = (
     ('FRIENDS', 'Friends'),
     ('COLLEAGUES', 'Colleagues'),
+)
+USER_STATUS = (
+    ('ACTIVE', 'Активный'),
+    ('INACTIVE','Заблокирован')
+)
+
+USER_PIC_TYPES = (
+    ('SOCIAL', 'Из социальных сетей'),
+    ('LOCAL', 'Сохранные изображения')
 )
