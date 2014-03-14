531629e8a4c049721493e92128929d4a08c35c38
Denis Lyubimov <dlyubimov@aaysm.com>
2014-03-14 17:55:52 +0400

    Added PersonsFilms class and migration for it

--- apps/films/constants.py
+++ apps/films/constants.py
@@ -4,13 +4,25 @@
 # Константы описывают тип фильма
 APP_FILM_FULL_FILM = 'FULL_FILM'
 APP_FILM_SERIAL    = 'SERIAL'
+APP_PERSON_ACTOR =''
+APP_PERSON_PRODUCER ='p'
+APP_PERSON_DIRECTOR = 'd'
 
 APP_FILM_FILM_TYPES = (
     (APP_FILM_FULL_FILM, u'Полнометражный фильм'),
     (APP_FILM_SERIAL, u'Сериал'),
 )
 
+
 APP_FILM_TYPE_ADDITIONAL_MATERIAL = (
     ('POSTER', u'Постер'),
     ('TRAILER', u'Трейлер')
 )
+
+
+APP_FILM_PERSON_TYPES = (
+    (APP_PERSON_ACTOR, u'Актер'),
+    (APP_PERSON_PRODUCER, u'Продюсер'),
+    (APP_PERSON_DIRECTOR, u'Режиссер'),
+)
+
