ef7af32d4035200b9a9f44ac2f2e3b846c8433ed
tumani1 <tumani1@yandex.ru>
2014-03-14 12:05:52 +0400

    - Добавил таблицу персон в админку

--- apps/users/admin.py
+++ apps/users/admin.py
@@ -3,22 +3,34 @@
 from django.contrib import admin
 from models import *
 
+
 #############################################################################################################
-# Администрирование пользователей
+# Администрирование таблицы пользователей
 class UsersAdmin(admin.ModelAdmin):
     list_display = ('id', 'firstname', 'firstname', 'email', 'created', 'ustatus')
     list_per_page = 30
+
 
 #############################################################################################################
 # Отношения пользователей
 class UsersRelsAdmin(admin.ModelAdmin):
     pass
 
+
+#############################################################################################################
+# Аминистрирование таблицы загружаемых картинок пользователей
 class UsersPicsAdmin(admin.ModelAdmin):
+    pass
+
+
+#############################################################################################################
+# Аминистрирование таблицы Персон
+class PersonsAdmin(admin.ModelAdmin):
     pass
 
 #############################################################################################################
 # Регистрация моделей в админке
 admin.site.register(Users, UsersAdmin)
 admin.site.register(UsersRels, UsersRelsAdmin)
-admin.site.register(UsersPics,UsersPicsAdmin)
+admin.site.register(UsersPics, UsersPicsAdmin)
+admin.site.register(Persons, PersonsAdmin)
