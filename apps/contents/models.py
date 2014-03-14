4d18542ad6300f8e454005e0cc41eb1698ab18d7
tumani1 <tumani1@yandex.ru>
2014-03-14 11:58:31 +0400

    - Добавил контента в settings(пока закомментировано) - Переименовал папку с контентом и поправил поля у моделей

--- apps/contents/models.py
+++ apps/contents/models.py
@@ -11,8 +11,8 @@ from apps.films.models import Films, Seasons
 class Content(models.Model):
     film                 = models.ForeignKey(Films, verbose_name=u'Фильм')
     name                 = models.CharField(max_length=255, verbose_name=u'Название')
-    name_orig            = models.CharField(max_length=255, verbose_name =u'Оригинальное название')
-    sNumber              = models.IntegerField(verbose_name=u'Номер сезона', null = True, blank =True)
+    name_orig            = models.CharField(max_length=255, verbose_name=u'Оригинальное название')
+    number               = models.IntegerField(null=True, blank=True, verbose_name=u'Номер сезона')
     description          = models.TextField(verbose_name=u'Описание')
     release_date         = models.DateTimeField(verbose_name=u'Дата выхода')
     season               = models.ForeignKey(Seasons, null=True, blank=True, verbose_name=u'Сезоны')
@@ -31,10 +31,10 @@ class Content(models.Model):
 #
 class Locations(models.Model):
     content    = models.ForeignKey(Content, verbose_name=u'Контент')
-    lType      = models.CharField(max_length=255, verbose_name=u'Тип')
+    ltype      = models.CharField(max_length=255, choices=[], verbose_name=u'Тип')
     quality    = models.CharField(max_length=40, verbose_name=u'Качество')
     subtitles  = models.CharField(max_length=40, verbose_name=u'Субтитры')
-    price      = models.FloatField(verbose_name=u'Цена')
+    price      = models.DecimalField(verbose_name=u'Цена')
     price_type = models.CharField(max_length=40, verbose_name=u'Тип цены')
     value      = models.CharField(max_length=40, verbose_name=u'Ценность')
     
@@ -50,9 +50,9 @@ class Locations(models.Model):
 class Comments(models.Model):
     user       = models.ForeignKey(Users, verbose_name=u'Пользователь')
     content    = models.IntegerField(verbose_name=u'Контент')
-    cText      = models.TextField(verbose_name=u'Tекст комментария')
+    ctext      = models.TextField(verbose_name=u'Tекст комментария')
     parent_id  = models.IntegerField(verbose_name=u'Родительский комментарий')
-    cStatus    = models.CharField(max_length=40, verbose_name=u'Статус')
+    cstatus    = models.CharField(max_length=40, verbose_name=u'Статус')
     created    = models.DateTimeField(auto_now_add=True, verbose_name=u'Создан')
     
     class Meta:
