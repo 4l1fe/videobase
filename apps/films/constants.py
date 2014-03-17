ff148ee6df111c5263d3384fd0f61fe6f45111c9
Maxim Pechnikov <mpechnikov@aaysm.com>
2014-03-17 12:21:56 +0400

    fixed char constants

--- apps/films/constants.py
+++ apps/films/constants.py
@@ -4,9 +4,9 @@
 # Константы описывают тип фильма
 APP_FILM_FULL_FILM = 'FULL_FILM'
 APP_FILM_SERIAL    = 'SERIAL'
-APP_PERSON_ACTOR =''
-APP_PERSON_PRODUCER ='p'
-APP_PERSON_DIRECTOR = 'd'
+APP_PERSON_ACTOR ='actor'
+APP_PERSON_PRODUCER ='producer'
+APP_PERSON_DIRECTOR = 'director'
 
 APP_FILM_FILM_TYPES = (
     (APP_FILM_FULL_FILM, u'Полнометражный фильм'),
@@ -25,4 +25,3 @@ APP_FILM_PERSON_TYPES = (
     (APP_PERSON_PRODUCER, u'Продюсер'),
     (APP_PERSON_DIRECTOR, u'Режиссер'),
 )
-
