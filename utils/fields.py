ba19830dfbc239d770803896987bf87a12d37b6f
tumani1 <tumani1@yandex.ru>
2014-03-17 12:43:26 +0400

    - добавил папку для утилит

---
+++ utils/fields.py
@@ -0,0 +1,36 @@
+# coding: utf-8
+
+from decimal import Decimal
+from django.db.models.fields import DecimalField
+
+
+#############################################################################################################
+# Собственное поле для цены
+class CurrencyField(DecimalField):
+    """
+    A CurrencyField is simply a subclass of DecimalField with a fixed format:
+    max_digits = 30, decimal_places=2, and defaults to 0.00
+    """
+    def __init__(self, **kwargs):
+        defaults = {
+            'max_digits': 30,
+            'decimal_places': 2,
+            'default': Decimal('0.0'),
+        }
+
+        defaults.update(kwargs)
+        super(CurrencyField, self).__init__(**defaults)
+
+    def south_field_triple(self):
+        """
+        Returns a suitable description of this field for South.
+        This is excluded from coverage reports since it is pretty much a piece
+        of South itself, and does not influence program behavior at all in
+        case we don't use South.
+        """
+        # We'll just introspect the _actual_ field.
+        from south.modelsinspector import introspector
+        field_class = "django.db.models.fields.DecimalField"
+        args, kwargs = introspector(self)
+
+        return (field_class, args, kwargs)
