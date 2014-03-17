6f9f29cf8bcef49c2b309dca430c85bc18f261c6
tumani1 <tumani1@yandex.ru>
2014-03-17 12:52:46 +0400

    - правка импорта моделей в приложении контента

--- apps/contents/admin.py
+++ apps/contents/admin.py
@@ -1,23 +1,30 @@
+# coding: utf-8
+
 from django.contrib import admin
 
-from apps.content.models import Content,Locations,Comments
+from apps.contents.models import *
 
 
+#############################################################################################################
+# Администрирование таблицы контента
 class ContentAdmin(admin.ModelAdmin):
     pass
 
+
+#############################################################################################################
+# Администрирование таблицы расположения контента
 class LocationsAdmin(admin.ModelAdmin):
     pass
 
+
+#############################################################################################################
+# Администрирование таблицы комментариев
 class CommentsAdmin(admin.ModelAdmin):
     pass
 
+
+#############################################################################################################
+# Регистрация моделей в админке
 admin.site.register(Content, ContentAdmin)
 admin.site.register(Locations, LocationsAdmin)
 admin.site.register(Comments, CommentsAdmin)
-
-        
-        
-
-
-
