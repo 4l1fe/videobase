a027b804618f089fe171db0cb59a02a02c60873a
tumani1 <tumani1@yandex.ru>
2014-03-19 16:13:38 +0400

    - url для статики в режиме DEBUG

--- videobase/settings.py
+++ videobase/settings.py
@@ -109,8 +109,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
-STATIC_URL = '/static/'
+MEDIA_ROOT = os.path.abspath(BASE_PATH + '/../static')
+MEDIA_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_PATH, '..', 'static')
+STATIC_URL = '/production/static/'
+STATIC_ROOT = os.path.join('/var/www/')
 
 SITE_ID = 1
