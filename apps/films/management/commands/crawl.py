21c2ce8257b1661ecac87d53477564b22113cc91
Denis Lyubimov <dlyubimov@aaysm.com>
2014-03-20 10:42:23 +0400

    Fixed bug with wrong reporting of success /fail

--- apps/films/management/commands/crawl.py
+++ apps/films/management/commands/crawl.py
@@ -4,7 +4,7 @@ from django.core.management.base import BaseCommand, CommandError
 from apps.films.models import Films,PersonsFilms,Persons
 from apps.films.constants import APP_PERSON_PHOTO_DIR,APP_FILM_CRAWLER_LIMIT,APP_FILM_CRAWLER_DELAY
 from apps.robots.models import KinopoiskTries
-from apps.robots.constants import APP_ROBOT_FAIL
+from apps.robots.constants import APP_ROBOT_FAIL, APP_ROBOT_SUCCESS
 from crawler.parse_page import parse_one_page
 from django.core.files import File
 from itertools import chain
@@ -61,21 +61,23 @@ class Command(BaseCommand):
 
                     film.kinopoisk_lastupdate = datetime.datetime.now()
                     film.save()
+                    for p in pdata['Persons']:
+                        po = get_person(film,p['name'])
+
+                        if 'photo' in p:
+                            if not (po.photo or (p['photo'] is None)):
+                                po.photo.save(os.path.join(APP_PERSON_PHOTO_DIR, str(po.id),'profile.jpg'),File(p['photo']))
+
+                        if PersonsFilms.objects.filter(film=film,person=po):
+                            pass
+                        else:
+                            pf = PersonsFilms(person = po , film = film, p_type = p['p_type'])
+                            pf.save()
+
                     kpt = KinopoiskTries(film = film,try_time = datetime.datetime.now(),result = APP_ROBOT_SUCCESS )
                     kpt.save()
-                except:
+
+                except Exception, e:
+                    print(e)
                     kpt = KinopoiskTries(film = film,try_time = datetime.datetime.now(),result = APP_ROBOT_FAIL )
                     kpt.save()
-                for p in pdata['Persons']:
-                    po = get_person(film,p['name'])
-
-                    if 'photo' in p:
-                        if not (po.photo or (p['photo'] is None)):
-                            po.photo.save(os.path.join(APP_PERSON_PHOTO_DIR, str(po.id),'profile.jpg'),File(p['photo']))
-
-                    if PersonsFilms.objects.filter(film=film,person=po):
-                        pass
-                    else:
-                        pf = PersonsFilms(person = po , film = film, p_type = p['p_type'])
-                        pf.save()
-
