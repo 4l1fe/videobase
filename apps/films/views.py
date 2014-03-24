12dcdf74063d1f4e6d12db72349be2fcae2c4b1d
Denis Lyubimov <dlyubimov@aaysm.com>
2014-03-24 18:45:55 +0400

    Added cut from image

--- apps/films/views.py
+++ apps/films/views.py
@@ -1,3 +1,71 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from apps.films.models import Persons,Films
+import re
+import os
+from PIL import Image, ImageEnhance
+from cStringIO import StringIO
+from django.core.files import File
 
 # Create your views here.
+
+def image_refresh(func):
+
+    def wrapper(request):
+
+        url = request.POST.get('image')
+        m = re.match('.+[/]static[/]upload[/](?P<type>[^/]+)[/](?P<id>[0-9]+)',url)
+
+        path = re.match('.+(?P<path>[/]static[/].+)',url)
+        d = m.groupdict()
+
+        p = Persons.objects.get(pk = int(d['id']))
+        im = Image.open('.'+path.groupdict()['path'])
+
+        imc = func(d,im,request)
+
+        imfile = StringIO()
+
+        imc.save(imfile,"PNG")
+        imfile.seek(0)
+
+        p.photo.save('.'+path.groupdict()['path']+'_resize.jpg', File(imfile))
+
+        return HttpResponse("OK")
+
+    return wrapper
+
+@image_refresh
+def resize_image(d,im,request):
+
+
+
+    x = int(request.POST.get('x'))
+    y = int(request.POST.get('y'))
+    x2 = int(request.POST.get('x2'))
+    y2 = int(request.POST.get('y2'))
+
+
+
+    imc = im.crop((x, y, x2, y2))
+
+    return imc
+
+@image_refresh
+def bri_con(d,im,request):
+
+
+    br = request.POST.get('br')
+    co = request.POST.get('co')
+    bre= ImageEnhance.Brightness(im)
+
+    imc =im
+    if br:
+        imc =bre.enhance(br/100.0)
+
+    con = ImageEnhance.Contrast(imc)
+    if co:
+        imc =con.enhance(co/100.0)
+
+    return imc
+    
