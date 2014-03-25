985f45b21b2c270c88249dbe6a44c8da43bab3f2
tumani1 <tumani1@yandex.ru>
2014-03-25 16:14:42 +0400

    - merge form - доп. валидация для модели FilmExtras

--- apps/films/models/films_extras.py
+++ apps/films/models/films_extras.py
@@ -1,6 +1,8 @@
 # coding: utf-8
 
 from django.db import models
+from django.core.exceptions import ValidationError, NON_FIELD_ERRORS
+
 from ..constants import *
 from apps.films.models.photoclass import PhotoClass
 from utils.common import get_image_path
@@ -15,14 +17,23 @@ class FilmExtras(PhotoClass):
     name        = models.CharField(max_length=255, verbose_name=u'Название')
     name_orig   = models.CharField(max_length=255, verbose_name=u'Оригинальное название')
     description = models.TextField(verbose_name=u'Описание')
-    url         = models.URLField(max_length=255, verbose_name=u'Ссылка на дополнительный материал')
+    url         = models.URLField(max_length=255, blank=True, null=True, verbose_name=u'Ссылка на дополнительный материал')
     photo       = models.ImageField(upload_to=get_image_path, blank=True, null=True, verbose_name=u'Постер')
 
+
+    def __init__(self, *args, **kwargs):
+        super(FilmExtras, self).__init__(*args, **kwargs)
+        self._original_etype = self.etype
 
     @property
     def get_upload_to(self):
         return APP_FILM_POSTER_DIR
 
+    def clean(self, *args, **kwargs):
+        if not self.pk is None:
+            if self._original_etype != self.etype:
+                msg = {'etype': (u"Это поле не изменяемо",)}
+                raise ValidationError(msg)
 
     def __unicode__(self):
         return u'[{0}] {1}'.format(self.pk, self.name)
