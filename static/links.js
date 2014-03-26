a5db0b636bbee6bf933190ca5fe33bd53670b1a4
Denis Lyubimov <dlyubimov@aaysm.com>
2014-03-26 10:14:44 +0400

    Added links to IMDB and Kinopoisk for films

--- static/links.js
+++ static/links.js
@@ -9,6 +9,12 @@ function add_name_links(){
     $('div.field-name').append('<br><div><a href= "http://google.com/search?q='+encodeURIComponent(name)+'">Поискать на Google</a></div>')
 
     $('div.field-name').append('<div><a href= "http://yandex.ru/yandsearch?text='+encodeURIComponent(name)+'">Поискать на Яндексе</a></div>')
+    var id_imdb_id =$("#id_imdb_id")[0].value;
+    $('div.field-imdb_id').append("<a href = 'http://www.imdb.com/title/tt0"+id_imdb_id +"/'> Страница фильма на IMDB</a>")
+
+    var id_kinopoisk_id =$("#id_kinopoisk_id")[0].value;
+    $('div.field-kinopoisk_id').append("<a href = 'http://www.kinopoisk.ru/film/"+id_kinopoisk_id +"/'> Страница фильма на Кинопоиске</a>")
+
 
 }
 
