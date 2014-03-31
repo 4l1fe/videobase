33bf69823d4c4e033fc1f7fa1b3dd710d13020d6
Ivan <wolko_dav@mail.ru>
2014-03-31 20:37:46 +0400

    Чек PEP8 Исправлен базовый класс загрузки html

--- crawler/core/loader.py
+++ crawler/core/loader.py
@@ -1,17 +1,28 @@
 # coding: utf-8
 
+from ..core import simple_get
+
 
 # Базовый класс загрузчика страници
 class BaseLoader(object):
     """ Base class to load html from site"""
-    def __init__(self, film, host, url_load):
+    def __init__(self, film, host, url_load, params=None):
         # Хост откуда скачиваем
         self.host = host
         # Наш фильм который ищим
         self.film = film
         # url для взятие фильма( шаблон)
         self.url_load = url_load
+        self.params = {} if params is None else params
+
+    # получить url для загрузки страници с фильмом
+    def get_url(self, load_function):
+        raise NotImplementedError()
 
     # сама функция загрузки
-    def load(self, load_function):
-        raise NotImplementedError()
+    def load(self, load_function=simple_get):
+        url = self.get_url(load_function)
+        html = load_function(url, params=self.params)
+        # Ошибка в случае загрузки страници
+        if html is None:
+            raise Exception()
