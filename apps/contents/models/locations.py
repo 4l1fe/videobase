f09a16eab86b72232255df637238fe1775bd0648
tumani1 <tumani1@yandex.ru>
2014-04-01 19:34:14 +0400

    - added fild lanf in the locations model

--- apps/contents/models/locations.py
+++ apps/contents/models/locations.py
@@ -3,7 +3,7 @@
 from django.db import models
 
 from utils.fields.currency_field import CurrencyField
-from apps.contents.constants import APP_CONTENTS_LOC_TYPE
+from apps.contents.constants import APP_CONTENTS_LOC_TYPE, APP_CONTENTS_PRICE_TYPE
 
 
 #############################################################################################################
@@ -11,10 +11,11 @@ from apps.contents.constants import APP_CONTENTS_LOC_TYPE
 class Locations(models.Model):
     content    = models.ForeignKey('Contents', verbose_name=u'Контент')
     type       = models.SmallIntegerField(choices=APP_CONTENTS_LOC_TYPE, verbose_name=u'Тип')
+    lang       = models.CharField(max_length=40, verbose_name=u'Язык')
     quality    = models.CharField(max_length=40, verbose_name=u'Качество')
     subtitles  = models.CharField(max_length=40, verbose_name=u'Субтитры')
     price      = CurrencyField(verbose_name=u'Цена')
-    price_type = models.SmallIntegerField(verbose_name=u'Тип цены')
+    price_type = models.SmallIntegerField(choices=APP_CONTENTS_PRICE_TYPE, verbose_name=u'Тип цены')
     value      = models.CharField(max_length=40, verbose_name=u'Ценность')
 
 
