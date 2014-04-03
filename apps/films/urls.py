f279175af0ba8f8589d361d4ebeee6171f3d0836
tumani1 <tumani1@yandex.ru>
2014-04-03 17:37:17 +0400

    - edited urls and requirements file

--- apps/films/urls.py
+++ apps/films/urls.py
@@ -1,11 +1,16 @@
 # coding: utf-8
 
-from rest_framework.urlpatterns import format_suffix_patterns
-from apps.films.api import DetailFilmView
 from django.conf.urls import *
 
-urlpatterns = patterns('',
+from rest_framework.urlpatterns import format_suffix_patterns
+
+from apps.films.api import DetailFilmView
+
+
+# Api patterns
+api_patterns = patterns('',
     url(r'^v1/film/(?P<film_id>\d+)?$', DetailFilmView.as_view(), name='film_detail_view')
 )
 
-urlpatterns = format_suffix_patterns(urlpatterns, suffix_required=True)
+# Format suffixes
+urlpatterns = format_suffix_patterns(api_patterns, suffix_required=True)
