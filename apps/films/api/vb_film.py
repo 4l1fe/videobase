ff6c697c55f3bcf56cf74f6a2625a2bca6c9b558
tumani1 <tumani1@yandex.ru>
2014-04-04 12:51:10 +0400

    - added extended fields for films api

--- apps/films/api/vb_film.py
+++ apps/films/api/vb_film.py
@@ -1,14 +1,16 @@
 # coding: utf-8
 
 from rest_framework import serializers
+import rest_framework.fields
 
-from apps.films.models import Films, FilmExtras, Countries
+from apps.films.models import Films, FilmExtras, Countries, Genres, PersonsFilms, Persons
 from apps.films.constants import APP_FILM_TYPE_ADDITIONAL_MATERIAL_POSTER
 
 from apps.contents.models import Contents, Locations
 
 
-
+#############################################################################################################
+#
 class CountriesSerializer(serializers.ModelSerializer):
 
     class Meta:
@@ -16,15 +18,56 @@ class CountriesSerializer(serializers.ModelSerializer):
         fields = ('id', 'name')
 
 
+#############################################################################################################
+#
+class GentriesSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Genres
+        fields = ('id', 'name')
+
+
+#############################################################################################################
+#
+class PersonsSerializer(serializers.ModelSerializer):
+
+    def __init__(self, *args, **kwargs):
+        new_fields = []
+        extend = kwargs.pop('extend', False)
+
+        if not extend:
+            new_fields += ['bio']
+
+        # Instantiate the superclass normally
+        super(PersonsSerializer, self).__init__(*args, **kwargs)
+
+        if len(new_fields):
+            # Drop keys if they exist
+            for field_name in new_fields:
+                self.fields.pop(field_name, None)
+
+
+    class Meta:
+        model = Persons
+        fields = ('id', 'name', 'photo', 'bio')
+
+
+#############################################################################################################
+#
 class LocationsSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Locations
+        exclude = ('content',)
 
 
+#############################################################################################################
+#
 class vbFilmSerializer(serializers.HyperlinkedModelSerializer):
     locations = LocationsSerializer()
-    # countries = CountriesSerializer()
+    countries = CountriesSerializer()
+    genres = GentriesSerializer()
+    persons = PersonsSerializer()
     ratings = serializers.SerializerMethodField('calc_ratings')
 
     def __init__(self, *args, **kwargs):
@@ -57,8 +100,9 @@ class vbFilmSerializer(serializers.HyperlinkedModelSerializer):
 
     class Meta:
         model = Films
-        fields = ['id', 'name', 'name_orig', 'release_date', 'description', 'ratings', 'duration',
-                  #'locations', 'relation', 'poster', 'locations', 'countries',
+        fields = ['id', 'name', 'name_orig', 'release_date', \
+                  'ratings', 'duration', #'locations', 'relation', 'poster',
+                  'description', 'countries', 'genres', 'persons',
                  ]
     #
     # def to_native(self, obj):
