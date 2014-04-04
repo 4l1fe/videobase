e23eb21aab5d0547c8fe760d98c002e74b2c16ea
tumani1 <tumani1@yandex.ru>
2014-04-04 11:06:56 +0400

    - detail api for film - location api for film

--- apps/films/api/__init__.py
+++ apps/films/api/__init__.py
@@ -1,7 +1,8 @@
 # coding: utf-8
 
 from films_search import FilmsSearchResource
-from get_film import DetailFilmView
+from films_detail import DetailFilmView
+from films_locations import LocationsFilmView
 
 
-__all__ = ['FilmsSearchResource', 'DetailFilmView']
+__all__ = ['FilmsSearchResource', 'DetailFilmView', 'LocationsFilmView']
--- apps/films/api/vb_film.py
+++ apps/films/api/vb_film.py
@@ -22,20 +22,48 @@ class LocationsSerializer(serializers.ModelSerializer):
         model = Locations
 
 
-
-
-
-class vbFilmSerializer(serializers.ModelSerializer):
-    # locations = LocationsSerializer()
+class vbFilmSerializer(serializers.HyperlinkedModelSerializer):
+    locations = LocationsSerializer()
     # countries = CountriesSerializer()
+    ratings = serializers.SerializerMethodField('calc_ratings')
+
+    def __init__(self, *args, **kwargs):
+        new_fields = []
+        extend = kwargs.pop('extend', False)
+
+        if not extend:
+            new_fields += ['description', 'genres', 'countries']
+
+        persons = kwargs.pop('persons', False)
+        if not persons:
+            new_fields += ['persons']
+
+        # Instantiate the superclass normally
+        super(vbFilmSerializer, self).__init__(*args, **kwargs)
+
+        if len(new_fields):
+            # Drop keys if they exist
+            for field_name in new_fields:
+                self.fields.pop(field_name, None)
+
+
+    def calc_ratings(self, obj):
+        return {
+            'imdb': [obj.rating_imdb, obj.rating_imdb_cnt],
+            'kp': [obj.rating_kinopoisk, obj.rating_kinopoisk_cnt],
+            'cons': [0, 0],
+        }
+
 
     class Meta:
         model = Films
-        fields = ['id', 'name', 'name_orig',\
-                  # 'release_date', 'poster', \
-                  # 'relation', 'ratings', 'duration', 'locations', 'countries',
-                  # 'description', \
+        fields = ['id', 'name', 'name_orig', 'release_date', 'description', 'ratings', 'duration',
+                  #'locations', 'relation', 'poster', 'locations', 'countries',
                  ]
+    #
+    # def to_native(self, obj):
+    #     del self.fields['name']
+    #     return obj
 
     def work(self):
         pass
