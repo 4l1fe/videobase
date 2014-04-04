21f01a062260f2f8925774100fd7373410b72653
Ivan <wolko_dav@mail.ru>
2014-04-04 19:29:30 +0400

    Удалена модель Users, вместо неё UsersProfile. Модель юзеров теперь django( дефолтная). Почищены каталоги.

--- videobase/urls.py
+++ videobase/urls.py
@@ -12,31 +12,16 @@ from apps.users.forms import CustomRegistrationForm
 admin.autodiscover()
 
 urlpatterns = patterns('',
-                       url(r'^admin/users/', include('apps.users.urls')),
                        url(r'^admin_tools/', include('admin_tools.urls')),
                        url(r'^admin/', include(admin.site.urls)),
                        url(r'^admin_tools/', include('admin_tools.urls')),
                        url(r'^admin/', include(admin.site.urls)),
-                       url(r'^api/image/resize/','apps.films.views.resize_image'),
-                       url(r'^api/image/brco/','apps.films.views.bri_con'),
+                       url(r'^api/image/resize/', 'apps.films.views.resize_image'),
+                       url(r'^api/image/brco/', 'apps.films.views.bri_con'),
                        url(r'^api/', include('apps.films.urls')),
-                       url(r'^api/robots/','apps.robots.views.schedule_api'),
-                       url(r'^robots/','apps.robots.views.schedule_interface'),
-                       url(r'^accounts/profile/', UserAccountView.as_view(), name='account_profile'),
+                       url(r'^api/robots/', 'apps.robots.views.schedule_api'),
+                       url(r'^robots/', 'apps.robots.views.schedule_interface'),
 )
-urlpatterns += patterns('',
-
-                        url(
-                            r'^accounts/register/$',
-                            RegistrationView.as_view(
-                                form_class=CustomRegistrationForm,
-                            ),
-                            name='registration_register',
-                        ),
-
-                        (r'^accounts/', include('registration.urls')),
-                        (r'^admin/', include(admin.site.urls)),
-                    )
 
 if settings.DEBUG:
     urlpatterns += patterns('',
