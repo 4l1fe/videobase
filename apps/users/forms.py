8d009a47a073b8abf4c63c6803102ebccf8b1259
Ivan <wolko_dav@mail.ru>
2014-04-07 21:04:47 +0400

    Добавлено создание пользователей. Востановление паролей для пользователей. Добавлены необходимые константы для социальной атентификации.

--- apps/users/forms.py
+++ apps/users/forms.py
@@ -1,12 +1,32 @@
 # coding: utf-8
 
-from django.forms import ModelForm
+from django import forms
 
-from .models import Users
+from .models import User, UsersProfile
 
 
-class CustomRegisterForm(ModelForm):
+class CustomRegisterForm(forms.ModelForm):
+    real_username = forms.CharField(label='Имя', max_length=128, required=False)
+
+    def __init__(self, **kwargs):
+        super(CustomRegisterForm, self).__init__(**kwargs)
+        self.fields['username'].required = False
+
+    def clean(self):
+        self.cleaned_data['username'] = self.cleaned_data['email']
+        return super(CustomRegisterForm, self).clean()
+
+    def save(self, commit=True):
+        instance = super(CustomRegisterForm, self).save(commit)
+        username = self.cleaned_data.get('real_username', None)
+        if username is not None:
+            UsersProfile.objects.create(username=username, user=instance)
+        return instance
 
     class Meta:
-        model = Users
-        fields = ['email', 'password', 'fio']
+        model = User
+        fields = ['email', 'password', 'username']
+        widgets = {
+            'password': forms.PasswordInput(),
+            'username': forms.HiddenInput(),
+        }
