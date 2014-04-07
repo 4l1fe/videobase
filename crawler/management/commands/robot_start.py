d567532680c19670f2f03b72425e6d7550b59813
Eugene <eshangin@aaysm.com>
2014-04-07 15:54:01 +0400

    Merge branch 'master' into playfamily
    Conflicts:
    	crawler/management/commands/robot_start.py

--- crawler/management/commands/robot_start.py
+++ crawler/management/commands/robot_start.py
@@ -179,6 +179,7 @@ class Command(BaseCommand):
         count = int(options['count'])
         
         film = Films.objects.filter(id__in=range(start, start + count + 1))
+        film = Films.objects.filter(id=1)
         site = options['site']
         logging.debug("Starting robot for %s", site)
         try:
