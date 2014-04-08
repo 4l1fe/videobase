1877eecf9860da65966ca263a815793d6f2024da
sergey.i <sipatov@aaysm.com>
2014-04-08 19:56:21 +0400

    Изменения робота megogo

--- crawler/megogo_net/loader.py
+++ crawler/megogo_net/loader.py
@@ -1,7 +1,8 @@
+# coding: utf-8
 from crawler.core.exceptions import NoSuchFilm
-import requests
+import string
 import parsers
-import urllib
+import re
 from ..core import BaseLoader
 
 HOST = 'www.megogo.net/ru'
@@ -12,7 +13,7 @@ class MEGOGO_Loader(BaseLoader):
     def __init__(self,  film, host=HOST, url_load=URL_LOAD):
         super(MEGOGO_Loader, self).__init__(film, host, url_load)
         self.search_url = URL_SEARCH
-        self.params = {'lang': 'ru', 'q': self.film.name}
+        self.params = {'lang': 'ru', 'q': re.sub('[' + string.punctuation + ']', '', self.film.name).lower().strip()}
 
     def get_url(self, load_function):
         url = "http://%s/%s" % (self.host, self.search_url, )
