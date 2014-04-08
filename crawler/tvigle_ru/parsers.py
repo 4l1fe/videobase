5d383338c23bffaa18c2f10bada3f3113c074544
Eugene <eshangin@aaysm.com>
2014-04-08 19:56:51 +0400

    Достает ссылку на фильм.

--- crawler/tvigle_ru/parsers.py
+++ crawler/tvigle_ru/parsers.py
@@ -1,16 +1,36 @@
 # coding: utf-8
 from ..core import BaseParse
 from bs4 import BeautifulSoup
-from social.backends.ubuntu import UbuntuOpenId
+import re
 
 URL_FILM = ''
 
 
 def parse_search(response, filmName):
+    s = u'Сезон'
+    mas = []
+    reg = re.compile('(?P<season>'+ s +')[ ](?P<number>\d+)')
     try:
         soup = BeautifulSoup(response.content)
-        tag = soup.find(attrs={'class':'vv vf cc4'})
+        tag_h4 = soup.findAll('h4')
+        if(tag_h4 == None):
+            return None
+        for tag in tag_h4:
+            tagA = tag.a
+            if tagA == None:
+                continue
+            seasonName = tagA.text
+            if filmName in seasonName:
+                if u'Сезон' in seasonName:
+                    search = reg.search(seasonName)
+                    if int(search.group('number')) in mas:
+                        continue
+                    mas.append(int(search.group('number')))
+                    print mas
+
+        tag = soup.find('h4',text=filmName)
         tagA = tag.a
+        filmLink = tagA.get('href')
 
     except:
         filmLink = None
@@ -23,18 +43,19 @@ class ParseTvigleFilm(object):
         pass
     def parse(self, response, dict_gen, film,url):
         d = dict_gen(film)
-        print url
         d['url_view'] = url
         d['price_type'] = 0
         d['price'] = self.get_price()
-        print d
         return  [d]
 
     def get_price(self):
         return 0
 
     def get_seasons(self):
-        return [0,]
+        return [0.]
 
     def get_link(self):
         pass
+
+
+
