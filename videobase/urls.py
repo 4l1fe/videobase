143c9bd2ffcdc1928f322dbc274228677a23df00
Ivan <wolko_dav@mail.ru>
2014-04-14 18:35:47 +0400

    Work with social network through OAuth

--- videobase/urls.py
+++ videobase/urls.py
@@ -15,14 +15,15 @@ urlpatterns = patterns('',
     url(r'^api/', include('apps.films.urls')),
     url(r'^api/', include('apps.users.api.urls')),
     url(r'^robots/', include('apps.robots.urls')),
-    url(r'^api/test',test_view),
+    url(r'^api/test', test_view),
     url(r'^users/', include('apps.users.urls')),
     url(r'^auth/login/', 'rest_framework.authtoken.views.obtain_auth_token'),
     
     # Admin
     url(r'^admin_tools/', include('admin_tools.urls')),
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^admin_tools/', include('admin_tools.urls')),
+    # Social
+    url(r'', include('social_auth.urls'))
 )
 
 if settings.DEBUG:
