3dde88ddf3c3e89c979e1a5f2632be15f752039a
Denis Lyubimov <dlyubimov@aaysm.com>
2014-04-14 17:34:23 +0400

    Removed Basic Authentication and cookies support. moved api_test.html

--- videobase/settings.py
+++ videobase/settings.py
@@ -159,8 +159,8 @@ REST_FRAMEWORK = {
     'rest_framework.renderers.JSONRenderer',
   ),
     'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework.authentication.BasicAuthentication',
-        'rest_framework.authentication.SessionAuthentication',
+        #'rest_framework.authentication.BasicAuthentication',
+        #'rest_framework.authentication.SessionAuthentication',
         'rest_framework.authentication.TokenAuthentication',
         'apps.users.models.api_session.MultipleTokenAuthentication',
     )
