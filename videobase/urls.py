d4c486edfc1029149c8210ed7514a772a1dbcfb5
tumani1 <tumani1@yandex.ru>
2014-04-14 18:25:17 +0400

    - fixed urls

--- videobase/urls.py
+++ videobase/urls.py
@@ -2,31 +2,24 @@
 
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
-import admin_tools
-
-from videobase import settings
-
-from django.contrib import admin
-from apps.films.views import test_view
 
 admin.autodiscover()
 
+from videobase import settings
+from apps.films.views import test_view
+
+
 urlpatterns = patterns('',
-                       url(r'^admin_tools/', include('admin_tools.urls')),
-                       url(r'^admin/', include(admin.site.urls)),
                        url(r'^admin_tools/', include('admin_tools.urls')),
                        url(r'^admin/', include(admin.site.urls)),
                        url(r'^api/image/resize/', 'apps.films.views.resize_image'),
                        url(r'^api/image/brco/', 'apps.films.views.bri_con'),
                        url(r'^api/', include('apps.films.urls')),
                        url(r'^api/', include('apps.users.api.urls')),
-                       url(r'^api/robots/', 'apps.robots.views.schedule_api'),
-                       url(r'^robots/', 'apps.robots.views.schedule_interface'),
+                       url(r'^robots/', include('apps.robots.urls')),
                        url(r'^api/test',test_view),
-                       url(r'^profile/', 'apps.users.views.profile_edit'),
                        url(r'^users/', include('apps.users.urls')),
                        url(r'^auth/login/', 'rest_framework.authtoken.views.obtain_auth_token'),
-
 )
 
 if settings.DEBUG:
