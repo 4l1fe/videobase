d1b14a41333ae3223564362d56e015dff4c52f3f
tumani1 <tumani1@yandex.ru>
2014-04-15 17:01:42 +0400

    - added search by locations

--- apps/films/api/films_search.py
+++ apps/films/api/films_search.py
@@ -10,6 +10,8 @@ from rest_framework import status
 from apps.films.api.serializers import vbFilm
 from apps.films.models import Films
 from apps.films.forms import SearchForm
+from apps.contents.models import Contents, Locations
+from apps.contents.constants import APP_CONTENTS_ONLINE_CINEMA
 
 
 #############################################################################################################
@@ -28,34 +30,36 @@ class SearchFilmsView(APIView):
 
     def parse_post(self, data):
         filter = {}
+        film_group = 0
+        location_group = 0
         if data.get('text'):
+            film_group += 1
             filter.update({'name': data['text']})
 
         if data.get('year_old'):
+            film_group += 1
             filter.update({'year_old': data['year_old']})
 
         if data.get('genre'):
+            film_group += 1
             filter.update({'genres': data['genre']})
 
         if data.get('rating'):
+            film_group += 1
             filter.update({'rating': data['rating']})
 
         if data.get('price'):
+            location_group += 1
             filter.update({'price': data['price']})
 
         if data.get('instock'):
+            location_group += 1
             filter.update({'instock': data['instock']})
 
-        return filter
+        return filter, film_group, location_group
 
 
-    def post(self, request, format=None, *args, **kwargs):
-        # Init data
-        page = request.DATA.get('page', 1)
-        per_page = request.DATA.get('per_page', 12)
-
-        filter = self.parse_post(request.DATA)
-
+    def search_by_films(self, filter):
         o_search = Films.objects.all()
         # Поиск по имени
         if filter.get('name'):
@@ -64,7 +68,7 @@ class SearchFilmsView(APIView):
         # Поиск по количеству прошедших лет
         if filter.get('year_old'):
             o_search = o_search.extra(
-                where=['extract(year from age(current_date, "films"."release_date")) >= %s'],
+                where=['EXTRACT(year FROM AGE(current_date, "films"."release_date")) >= %s'],
                 params=[filter['year_old']],
             )
 
@@ -75,6 +79,60 @@ class SearchFilmsView(APIView):
         # Поиск по жанрам
         if filter.get('genres'):
             o_search = o_search.filter(genres=filter['genres'])
+
+        return o_search
+
+
+    def search_by_location(self, filter, o_search=None):
+        o_loc = Locations.objects.all()
+
+        if not o_search is None:
+            list_film_pk = set(o_search.values_list('id', flat=True))
+            conts_dict = dict(Contents.objects.filter(film__in=list_film_pk).values_list('id', 'film'))
+            o_loc = o_loc.filter(content__in=conts_dict.keys())
+
+        o_loc = o_loc.distinct('content').values_list('content', flat=True)
+
+        if filter.get('price'):
+            o_loc = o_loc.filter(price__gte=filter['price'])
+
+        if filter.get('instock'):
+            o_loc = o_loc.filter(type=APP_CONTENTS_ONLINE_CINEMA)
+
+        # Пересечение
+        if o_search is None:
+            list_films_by_content = Contents.objects.filter(pk__in=o_loc).\
+                values_list('film', flat=True).distinct('film')
+        else:
+            list_films_by_content = list_film_pk & set([conts_dict[i] for i in o_loc if i in conts_dict])
+
+        return list_films_by_content
+
+
+    def post(self, request, format=None, *args, **kwargs):
+        # Init data
+        page = request.DATA.get('page', 1)
+        per_page = request.DATA.get('per_page', 12)
+
+        filter, film_group, location_group = self.parse_post(request.DATA)
+
+        if film_group > 0:
+            o_search = self.search_by_films(filter)
+
+            list_films_by_content = []
+            if location_group > 0:
+                list_films_by_content = self.search_by_location(filter, o_search)
+
+            # Пересечение не пусто
+            if len(list_films_by_content):
+                o_search = Films.objects.filter(pk__in=list_films_by_content)
+
+        else:
+            list_films_by_content = []
+            if location_group > 0:
+                list_films_by_content = self.search_by_location(filter)
+
+            o_search = Films.objects.filter(pk__in=list_films_by_content)
 
         try:
             page = Paginator(o_search, per_page=per_page).page(page)
