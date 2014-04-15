1e9c81e596a1bfbb5e1ea0ad3e96c1334cbb4172
Eugene <eshangin@aaysm.com>
2014-04-15 19:36:06 +0400

    Merge branch 'master' into test_persons_api
    Conflicts:
    	apps/contents/migrations/0003_rename_fields.py
    	apps/users/tests.py
    	crawler/management/commands/robot_start.py

--- apps/films/tests/factories.py
+++ apps/films/tests/factories.py
@@ -4,16 +4,52 @@ __author__ = 'eugene'
 
 
 import factory
-from apps.films.models import Persons
+from apps.films.models import Persons, PersonsFilms, Films
+import datetime
 
 
-class PersonFactory(factory.Factory):
+class PersonFactory(factory.DjangoModelFactory):
 
     FACTORY_FOR = Persons
-    name = u'Серж'
-    name_orig = u'Serj'
-    bio = u'Сергей черепашка обожатель'
-    photo = u'asdasd'
+    name = u'Иван'
+    name_orig = u'Ivan'
+    bio = u'Иван иваныч'
+    photo = ''
     id = 1
-    extend = False
+
+
+class FilmFactory(factory.DjangoModelFactory):
+    FACTORY_FOR = Films
+    FACTORY_DJANGO_GET_OR_CREATE = ('name',)
+    pk = factory.Sequence(lambda n: n)
+    id = factory.Sequence(lambda n: n)
+    name = factory.Sequence(lambda n: u'Фильм{0}'.format(n))
+    type = u'FULL_FILM'
+    release_date = datetime.date(2014, 3, 21)
+    description = u'Боевик'
+    name_orig = factory.Sequence(lambda n: u'Film{0}'.format(n))
+
+
+class PersonsFilmography(factory.DjangoModelFactory):
+
+    FACTORY_FOR = PersonsFilms
+    film = factory.SubFactory(FilmFactory)
+    person = factory.SubFactory(PersonFactory)
+    p_type = ''
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
--- apps/films/tests/test_persons_api.py
+++ apps/films/tests/test_persons_api.py
@@ -1,4 +1,4 @@
-from apps.films.tests.factories import PersonFactory
+from apps.films.tests.factories import PersonFactory, FilmFactory, PersonsFilmography
 from apps.films.views import PersonAPIView
 from rest_framework.test import APIRequestFactory
 from rest_framework.test import APIClient
@@ -8,11 +8,40 @@ from rest_framework import status
 from rest_framework.test import APITestCase
 
 
-
 class PersonsTest(APITestCase):
-    def test_view(self):
-        #person = PersonFactory.create()
-        # response = self.client.get('/person/1')
-        self.assertTrue(True)
-        # self.assertEqual(response.status_code, status.HTTP_200_OK)
-        # self.assertContains(response, person.name)
+    def setUp(self):
+        self.person = PersonFactory.create()
+        #self.film = FilmFactory.create()
+        #self.person_filmography = PersonsFilmography.create()
+
+    def test_person_view_ok(self):
+        response = self.client.get(reverse('person_api_view', kwargs={'resource_id': self.person.id, 'format': 'json'}))
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_person_view_404(self):
+        response = self.client.get(reverse('person_api_view', kwargs={'resource_id': 0, 'format': 'json'}))
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
+    def test_person_api_view(self):
+        response = self.client.get(reverse('person_api_view', kwargs={'resource_id': self.person.id, 'format': 'json'}))
+        self.assertEqual(response.data['id'], self.person.id)
+        self.assertEqual(response.data['photo'], self.person.photo)
+        self.assertEqual(response.data['name'], self.person.name)
+
+    def test_person_filmography_view_ok(self):
+        response = self.client.get(reverse('person_filmography_view', kwargs={'resource_id': self.person.id, 'format': 'json'}))
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_person_filmography_view_404(self):
+        response = self.client.get(reverse('person_filmography_view', kwargs={'resource_id': 0, 'format': 'json'}))
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
+    def test_person_filmography_view(self):
+        response = self.client.get(reverse('person_filmography_view', kwargs={'resource_id': self.person.id, 'format': 'json'}))
+        print response.data
+
+
+
+
+
+
