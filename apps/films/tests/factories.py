a5104a0622808677de97e0b9c0aa6b85e71e97b0
sergey.i <sipatov@aaysm.com>
2014-04-16 20:04:08 +0400

    тесты API фильмов 1.2

--- apps/films/tests/factories.py
+++ apps/films/tests/factories.py
@@ -6,19 +6,26 @@ __author__ = 'eugene'
 import factory
 import datetime
 from apps.contents.models import Contents, Locations
-from apps.films.models import Persons, Films
+from apps.films.models import Persons, Films, Genres
 
 
 class FilmFactory(factory.DjangoModelFactory):
     FACTORY_FOR = Films
     FACTORY_DJANGO_GET_OR_CREATE = ('name',)
     pk = factory.Sequence(lambda n: n)
-    #id = factory.Sequence(lambda n: n)
     name = factory.Sequence(lambda n: u'Фильм{0}'.format(n))
     type = u'FULL_FILM'
     release_date = datetime.date(2014, 3, 21)
     description = u'Боевик'
     name_orig = factory.Sequence(lambda n: u'Film{0}'.format(n))
+    @factory.post_generation
+    def genres(self, create, extracted, **kwargs):
+        if not create:
+            return
+
+        if extracted:
+            for genre in extracted:
+                self.genres.add(genre)
 
 
 
@@ -34,8 +41,8 @@ class ContentFactory(factory.DjangoModelFactory):
 
 
 class LocationFactory(factory.DjangoModelFactory):
-    pk = factory.Sequence(lambda m: m)
     FACTORY_FOR = Locations
+    pk = factory.Sequence(lambda m: m)
     content = factory.SubFactory(ContentFactory)
     type = APP_CONTENTS_ONLINE_CINEMA
     lang = u'eng'
@@ -44,6 +51,14 @@ class LocationFactory(factory.DjangoModelFactory):
     url_view = u'http://www.megogo.net/item/Red.html'
     quality = u''
     subtitles = u''
+
+
+class GenreFactory(factory.DjangoModelFactory):
+    pk = factory.Sequence(lambda b: b)
+    FACTORY_FOR = Genres
+    name = factory.Sequence(lambda c: u'Жанр{0}'.format(c))
+    description = factory.Sequence(lambda a: u'Описание Жанра_{0}'.format(a))
+
 
 
 
--- apps/films/tests/tests_films_api.py
+++ apps/films/tests/tests_films_api.py
@@ -3,7 +3,7 @@ from apps.films.api import DetailFilmView
 from apps.films.api.serializers import vbFilm
 
 __author__ = 'ipatov'
-from apps.films.tests.factories import ContentFactory, FilmFactory, LocationFactory
+from apps.films.tests.factories import ContentFactory, FilmFactory, LocationFactory, GenreFactory
 from apps.films.views import PersonAPIView
 from rest_framework.test import APIRequestFactory, APITestCase
 from rest_framework.test import APIClient
@@ -17,36 +17,57 @@ import json
 
 class FilmsTest(APITestCase):
     """
-    Метод  post не тестируем, т.к. там те же действия что и в get
+    Метод  post в film_datail не тестируем, т.к. там те же действия что и в get
     """
     def setUp(self):
-        self.location = LocationFactory.create()
+        self.genres = []
+        self.locations = []
+        self.films = []
+        for i in range(4):
+            self.genres.append(GenreFactory.create())
+        for i in range(2):
+            self.locations.append(LocationFactory.create(content__film__genres=(self.genres[2*i], self.genres[2*i+1])))
+        for location in self.locations:
+            self.films.append(location.content.film)
 
     def test_api_detail_ok(self):
-        response = self.client.get(reverse('film_details_view', kwargs={'film_id': self.content.film.id, 'format': 'json'}))
-        print response
+        film = self.films[0]
+        response = self.client.get(reverse('film_details_view', kwargs={'film_id': film.id, 'format': 'json'}))
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
     def test_api_detail_data(self):
-        response = self.client.get(reverse('film_details_view', kwargs={'film_id': self.content.film.id, 'format': 'json'}))
-        print response.data['content']['film'].__dict__
-        self.assertEqual(response.data['id'], self.film.id)
-        self.assertEqual(response.data['name'], self.film.name)
-        self.assertEqual(response.data['name_orig'], self.film.name_orig)
-        self.assertEqual(response.data['release_date'], self.film.release_date)
-        self.assertEqual(response.data['description'], self.film.description)
-        self.assertEqual(response.data['ratings']['kp'][0], self.film.rating_kinopoisk)
-        self.assertEqual(response.data['ratings']['kp'][1], self.film.rating_kinopoisk_cnt)
-        self.assertEqual(response.data['ratings']['imdb'][0], self.film.rating_imdb)
-        self.assertEqual(response.data['ratings']['imdb'][1], self.film.rating_imdb_cnt)
+        film = self.films[0]
+        location = None
+        for loc in self.locations:
+            if film.id == loc.content.film.id:
+                location = loc
+                break
+        response = self.client.get(reverse('film_details_view', kwargs={'film_id': film.id, 'format': 'json'}))
+        self.assertEqual(response.data['id'], film.id)
+        self.assertEqual(response.data['name'], film.name)
+        self.assertEqual(response.data['name_orig'], film.name_orig)
+        self.assertEqual(response.data['release_date'], film.release_date)
+        self.assertEqual(response.data['description'], film.description)
+        self.assertEqual(response.data['ratings']['kp'][0], film.rating_kinopoisk)
+        self.assertEqual(response.data['ratings']['kp'][1], film.rating_kinopoisk_cnt)
+        self.assertEqual(response.data['ratings']['imdb'][0], film.rating_imdb)
+        self.assertEqual(response.data['ratings']['imdb'][1], film.rating_imdb_cnt)
         self.assertEqual(response.data['ratings']['cons'][0], 0)
         self.assertEqual(response.data['ratings']['cons'][1], 0)
-        self.assertEqual(response.data['duration'], self.film.duration)
+        self.assertEqual(response.data['duration'], film.duration)
         self.assertEqual(response.data['poster'], [])
         self.assertEqual(response.data['countries'], [])
-        self.assertEqual(response.data['genres'], [])
+        self.assertEqual(response.data['genres'][0], film.genres.all().values('id', 'name')[0])
         self.assertEqual(response.data['persons'], [])
-        self.assertEqual(response.data['locations'], [])
+        self.assertEqual(response.data['locations']['id'], location.id)
+        self.assertEqual(response.data['locations']['content'], location.content.id)
+        self.assertEqual(response.data['locations']['type'], location.type)
+        self.assertEqual(response.data['locations']['lang'], location.lang)
+        self.assertEqual(response.data['locations']['quality'], location.quality)
+        self.assertEqual(response.data['locations']['subtitles'], location.subtitles)
+        self.assertEqual(response.data['locations']['price'], location.price)
+        self.assertEqual(response.data['locations']['price_type'], location.price_type)
+        self.assertEqual(response.data['locations']['url_view'], location.url_view)
         self.assertEqual(response.data['relation'], [])
 
     def test_api_detail_404(self):
@@ -54,6 +75,7 @@ class FilmsTest(APITestCase):
         self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
 
     def test_api_search(self):
-        data = {'text': u'RED5'}
+        film = self.films[0]
+        data = {'text': film.name}
         response = self.client.post(reverse('film_search_view', kwargs={'format': 'json'}), data=data)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
