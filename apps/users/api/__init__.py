bcc17f435dc19687716c3d60a059f64189520cc2
Ivan <wolko_dav@mail.ru>
2014-04-17 17:10:19 +0400

    Check PEP8. Add all users API without users_friends and users_feed. And optimization db querys

--- apps/users/api/__init__.py
+++ apps/users/api/__init__.py
@@ -1,5 +1,10 @@
 # coding: utf-8
-
-from users_action_friendship import UsersFriendshipView
-from users_films import UsersFilmsView
+# User API
 from user_info import UserInfoView, UserChangePasswordView
+# Users API
+from users import UsersView
+from users_films import UsersFilmsView
+from users_persons import UsersPersonsView
+from users_friends import UsersFriendsView
+from users_genres import UsersGenresView
+from users_action_friendship import UsersFriendshipView
