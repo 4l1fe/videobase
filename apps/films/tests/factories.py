3a6b331b7d3116f4efec4017a92be609081fabfd
Eugene <eshangin@aaysm.com>
2014-04-18 17:18:13 +0400

    Merge branch 'master' into test_persons_api
    Conflicts:
    	apps/contents/migrations/0003_rename_fields.py
    	apps/users/tests.py
    	crawler/management/commands/robot_start.py

--- apps/films/tests/factories.py
+++ apps/films/tests/factories.py
@@ -4,25 +4,30 @@ __author__ = 'eugene'
 
 
 import factory
-from apps.films.models import Persons, PersonsFilms, Films
+from apps.films.models import Persons, PersonsFilms, Films, UsersPersons
+from rest_framework.authtoken.models import Token
 import datetime
+from django.contrib.auth.models import User
+import random
+
 
 
 class PersonFactory(factory.DjangoModelFactory):
 
     FACTORY_FOR = Persons
-    name = u'Иван'
-    name_orig = u'Ivan'
-    bio = u'Иван иваныч'
+    name = u'Nicolas Cage'
+    name_orig = u'Nicolas Cage'
+    bio = u''
     photo = ''
     id = 1
+    pk = factory.Sequence(lambda n: n)
 
 
 class FilmFactory(factory.DjangoModelFactory):
     FACTORY_FOR = Films
     FACTORY_DJANGO_GET_OR_CREATE = ('name',)
     pk = factory.Sequence(lambda n: n)
-    id = factory.Sequence(lambda n: n)
+    id = 1
     name = factory.Sequence(lambda n: u'Фильм{0}'.format(n))
     type = u'FULL_FILM'
     release_date = datetime.date(2014, 3, 21)
@@ -33,9 +38,31 @@ class FilmFactory(factory.DjangoModelFactory):
 class PersonsFilmography(factory.DjangoModelFactory):
 
     FACTORY_FOR = PersonsFilms
-    film = factory.SubFactory(FilmFactory)
     person = factory.SubFactory(PersonFactory)
+    film = factory.SubFactory(FilmFactory)
     p_type = ''
+    p_character = ''
+    description = ''
+
+
+class UsersPersonsFactory(factory.DjangoModelFactory):
+
+    FACTORY_FOR = UsersPersons
+    person = factory.SubFactory(PersonFactory)
+
+
+class UserFactory(factory.DjangoModelFactory):
+
+    FACTORY_FOR = User
+    pk = factory.Sequence(lambda n: n)
+    username = factory.Sequence(lambda n: u'admin{0}'.format(n))
+    password = u'admin'
+
+
+
+
+
+
 
 
 
