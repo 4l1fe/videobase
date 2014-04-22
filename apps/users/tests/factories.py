5b68db60b47439c06b9ba618f90170988f52f098
Ivan <wolko_dav@mail.ru>
2014-04-22 18:54:35 +0400

    Merge remote-tracking branch 'origin/test_films_api' into test_user_api
    Conflicts:
    	apps/users/tests/factories.py
    	apps/users/tests/tests_users_api.py

--- apps/users/tests/factories.py
+++ apps/users/tests/factories.py
@@ -7,17 +7,13 @@ from factory.django import ImageField
 
 class UserFactory(factory.DjangoModelFactory):
     FACTORY_FOR = User
-    # pk = factory.Sequence(lambda n: n)
     username = factory.Sequence(lambda n: u'admin{0}'.format(n))
     password = u'admin'
 
 
 class UserProfileFactory(factory.DjangoModelFactory):
     FACTORY_FOR = UsersProfile
-    # pk = factory.Sequence(lambda n: n)
 
 
 class UserPicsFactory(factory.DjangoModelFactory):
     FACTORY_FOR = UsersPics
-    # pk = factory.sequence(lambda n: n)
-    
