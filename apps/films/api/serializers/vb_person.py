932f1b263e8f850de3a62a0a45610ede29b4e1fa
4l1fe <4l1fe@users.noreply.github.com>
2014-04-25 19:12:22 +0400

    c1

--- apps/films/api/serializers/vb_person.py
+++ apps/films/api/serializers/vb_person.py
@@ -1,28 +1,36 @@
 # coding: utf-8
 
 from rest_framework import serializers
-from apps.films.models import Persons
+from apps.films.models import Persons, PersonsFilms
 
 
 #############################################################################################################
 #
 class vbPerson(serializers.ModelSerializer):
     birthplace = serializers.SerializerMethodField('get_birthplace')
+    relation = serializers.SerializerMethodField('get_relation')
+    roles = serializers.SerializerMethodField('get_roles')
 
-    def __init__(self, *args, **kwargs):
-        new_fields = []
+    def __init__(self, *args, **kwargs):  # TODO: подготовить список городов, стран для получения места рождения
+                                          # TODO: подготовить список ролей, для получения их по каждой персоне
+        extended_fields = []
 
         extend = kwargs.pop('extend', False)
         if not extend:
-            new_fields += ['bio']
+            extended_fields += ['bio', 'roles']
 
         # Instantiate the superclass normally
         super(vbPerson, self).__init__(*args, **kwargs)
 
-        if len(new_fields):
+        if len(extended_fields):
             # Drop keys if they exist
-            for field_name in new_fields:
+            for field_name in extended_fields:
                 self.fields.pop(field_name, None)
+
+        self.prepared_roles = self._get_roles()
+
+    def get_relation(self, obj):
+        pass
 
     def get_birthplace(self, obj):
         if not obj.city is None:
@@ -31,6 +39,15 @@ class vbPerson(serializers.ModelSerializer):
             return [city, country]
         return []
 
+    def _get_roles(self):
+        persons_list = self.object
+        person_roles = PersonsFilms.objects.filter(person__in=persons_list).distinct('person', 'p_type')
+
+    def get_roles(self, obj):
+        if obj.person_film_rel is not None:
+            return (pf.p_type for pf in obj.person_film_rel.distinct('p_type'))
+        return []
+
     class Meta:
         model = Persons
-        fields = ('id', 'name', 'photo', 'bio', 'birthplace')
+        fields = ('id', 'name', 'photo', 'relation', 'birthdate', 'birthplace', 'bio', 'roles')
