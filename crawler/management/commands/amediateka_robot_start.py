88c3e130feea25dfd7f33df4f1f7bd05ae4613ff
Eugene <eshangin@aaysm.com>
2014-04-25 17:28:38 +0400

    Merge branch 'test_films_api' into test_persons_api
    Conflicts:
    	apps/films/tests/factories.py
    	apps/films/tests/test_persons_api.py
    	apps/films/tests/tests_films_api.py
    	fabfile.py

---
+++ crawler/management/commands/amediateka_robot_start.py
@@ -0,0 +1,11 @@
+from crawler.amediateka_ru.loader import Amediateka_robot
+from django.core.management.base import NoArgsCommand
+from apps.robots.models import  RobotsTries, Robots
+from crawler.robot_start import launch_next_robot_try as handle, sites_crawler
+from crawler.amediateka_ru.loader import  *
+
+class Command(NoArgsCommand):
+    def handle_noargs(self, **options):
+        am_load = Amediateka_robot()
+        am_load.get_data()
+
