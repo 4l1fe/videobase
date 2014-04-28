813221ede59ccbd3b9d21c0e726ac9c78c4611e8
Denis Lyubimov <dlyubimov@aaysm.com>
2014-04-28 17:14:29 +0400

    Fixed address

--- crawler/constants.py
+++ crawler/constants.py
@@ -2,5 +2,5 @@ RATING_GZIP_FILE_URL = "http://ftp.sunet.se/pub/tv+movies/imdb/ratings.list.gz"
 PAGE_ARCHIVE = './page_dumps/'
 SUPERVISOR_XMLRPC = 'http://localhost:9001/RPC2'
 USE_SOCKS5_PROXY=False
-SOCKS5_PROXY_ADDRESS = 'http://socks5://127.0.0.1:5555'
+SOCKS5_PROXY_ADDRESS = 'socks5://127.0.0.1:5555'
 
