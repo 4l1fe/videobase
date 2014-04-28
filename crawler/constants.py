6d90e1ae0a0ebe9c97a9ce5c10349a1ec680a464
Denis Lyubimov <dlyubimov@aaysm.com>
2014-04-28 16:59:36 +0400

    Added proxy handling for kinopoisk robot

--- crawler/constants.py
+++ crawler/constants.py
@@ -1,5 +1,6 @@
 RATING_GZIP_FILE_URL = "http://ftp.sunet.se/pub/tv+movies/imdb/ratings.list.gz"
 PAGE_ARCHIVE = './page_dumps/'
 SUPERVISOR_XMLRPC = 'http://localhost:9001/RPC2'
-
+USE_SOCKS5_PROXY=False
+SOCKS5_PROXY_ADDRESS = 'http://socks5://127.0.0.1:5555'
 
