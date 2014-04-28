d1a6e6d7abd227e462d6c7f97f670110b60803f0
Eugene <eshangin@aaysm.com>
2014-04-28 15:42:29 +0400

    Каркас для робата tvzavr_ru

---
+++ crawler/management/commands/tvzavr_robot_start.py
@@ -0,0 +1,7 @@
+from django.core.management.base import BaseCommand
+from crawler.robot_start import launch_next_robot_try
+
+
+class Command(BaseCommand):
+    def handle(self, *args, **kwargs):
+        launch_next_robot_try('tvzavr_ru', 1)
