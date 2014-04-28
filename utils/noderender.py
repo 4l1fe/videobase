83ff35323c0c9bb8dcc4a0a441c4e5989962fc89
Denis Lyubimov <dlyubimov@aaysm.com>
2014-04-28 11:14:32 +0400

    Fixed film page

--- utils/noderender.py
+++ utils/noderender.py
@@ -2,11 +2,8 @@ from subprocess import PIPE, Popen
 import json
 
 
-
-
 def render_page(page_type,context):
         render_proc = Popen(['nodejs', 'renderproc.js'], stdin=PIPE, stdout=PIPE)
-        print context
         html,status = render_proc.communicate(json.dumps({
             "template": page_type,
             "context": context
