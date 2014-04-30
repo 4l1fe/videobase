8545e7c42e187d8e294a9b127a76848765fd7db5
sergey.i <sipatov@aaysm.com>
2014-04-30 20:19:40 +0400

    робот play.google.com

--- crawler/management/commands/stream_robot_test.py
+++ crawler/management/commands/stream_robot_test.py
@@ -4,4 +4,4 @@ from crawler.robot_start import launch_next_robot_try
 
 class Command(BaseCommand):
     def handle(self, *args, **kwargs):
-        launch_next_robot_try('stream_ru', 25)
+        launch_next_robot_try('play_google_com', 1)
