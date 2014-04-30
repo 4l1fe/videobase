79893409747b03ce3e9fcb25940af5fedc1dbf7f
Ivan <wolko_dav@mail.ru>
2014-04-30 17:04:41 +0400

    Add robot to nichego.me/movies/ to get poster

--- crawler/management/commands/list_robots.py
+++ crawler/management/commands/list_robots.py
@@ -1,9 +1,10 @@
 from apps.robots.models import Robots
 from django.core.management.base import BaseCommand
 
+
 class Command(BaseCommand):
 
-    def handle(self,*args,**kwargs):
+    def handle(self, *args, **kwargs):
 
         for robot in Robots.objects.all():
             print robot.name
