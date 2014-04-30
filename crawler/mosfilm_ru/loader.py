104eee2a0185d023865a83a4eb511916a2c2ddf9
Eugene <eshangin@aaysm.com>
2014-04-30 19:00:26 +0400

    Каркас для робота mosfilm.ru

--- crawler/mosfilm_ru/loader.py
+++ crawler/mosfilm_ru/loader.py
@@ -1,1 +1,14 @@
+# coding: utf-8
+import urllib
+from crawler.core import BaseLoader
+URL_LOAD = ''
+HOST = 'http://www.mosfilm.ru'
 
+
+class Mosfilm_Loader(BaseLoader):
+    def __init__(self, film, host=HOST, url_load=URL_LOAD):
+        super(Mosfilm_Loader, self).__init__(film, host, url_load)
+        self.url_serch = 'search/?{}'.format(urllib.urlencode({'b_search_words': u'Девчата', 'p_f_1_title': u'Девчата', 'p_f_2_title': u'Девчата'}))
+
+    def get_url(self, load_function):
+        print self.url_serch
