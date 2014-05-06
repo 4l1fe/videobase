0b7abab5e1ad4b2b4b97e77ae7279c105b9e4d13
sergey.i <sipatov@aaysm.com>
2014-05-06 14:31:49 +0400

    робот ayyo.ru переделанный

--- crawler/ayyo_ru/robot.py
+++ crawler/ayyo_ru/robot.py
@@ -18,17 +18,20 @@ class AyyoRobot(object):
         self.response = requests.get(url, headers={"Accept": "application/vnd.ayyo.api+json; version=1.2", "Authorization": "Bearer 1db6377876"})
 
     def get_data(self):
-        films = json.loads(self.response.content)['live_search']['search_movies_result']
-        for film in films:
-            if film['rus_title'] == self.film.name:
-                film_link = 'https://www.ayyo.ru/movies/%s/' % (film['slug'])
-                ayyo_film_id = film['movie']
-                break
-        film_url = 'https://www.ayyo.ru/api/movies/?{}'.format(urllib.urlencode({'id__in': ayyo_film_id}))
-        film_response = requests.get(film_url, headers={"Accept": "application/vnd.ayyo.api+json; version=1.2", "Authorization": "Bearer 1db6377876"})
-        price = float(json.loads(film_response.content)['movies']['data'][str(ayyo_film_id)]['streaming_price'])
-        d = self.film_dict(self.film, film_link, price)
-        save_location(**d)
+        try:
+            films = json.loads(self.response.content)['live_search']['search_movies_result']
+            for film in films:
+                if film['rus_title'] == self.film.name:
+                    film_link = 'https://www.ayyo.ru/movies/%s/' % (film['slug'])
+                    ayyo_film_id = film['movie']
+                    break
+            film_url = 'https://www.ayyo.ru/api/movies/?{}'.format(urllib.urlencode({'id__in': ayyo_film_id}))
+            film_response = requests.get(film_url, headers={"Accept": "application/vnd.ayyo.api+json; version=1.2", "Authorization": "Bearer 1db6377876"})
+            price = float(json.loads(film_response.content)['movies']['data'][str(ayyo_film_id)]['streaming_price'])
+            d = self.film_dict(self.film, film_link, price)
+            save_location(**d)
+        except Exception, e:
+            pass
 
     def film_dict(self, film, film_link, price):
         if price == 0:
@@ -36,6 +39,7 @@ class AyyoRobot(object):
         else:
             price_type = APP_CONTENTS_PRICE_TYPE_PAY
         resp_dict = sane_dict(film)
+        resp_dict['number'] = 0
         resp_dict['url_view'] = film_link
         resp_dict['price_type'] = price_type
         resp_dict['price'] = price
