6aab86d6f0fa044b5ff51e215d962241931fbd1a
Eugene <eshangin@aaysm.com>
2014-05-06 17:42:17 +0400

    Закончил робота mosfilm

--- crawler/mosfilm_ru/loader.py
+++ crawler/mosfilm_ru/loader.py
@@ -1,14 +1,36 @@
 # coding: utf-8
 import urllib
-from crawler.core import BaseLoader
+import urllib2
+from apps.films.models import Films
+from crawler.core.exceptions import NoSuchFilm
+from crawler.mosfilm_ru.parser import parse_search
+from crawler.robot_start import sane_dict, save_location
+
+
 URL_LOAD = ''
-HOST = 'http://www.mosfilm.ru'
+HOST = 'cinema.mosfilm.ru'
 
 
-class Mosfilm_Loader(BaseLoader):
-    def __init__(self, film, host=HOST, url_load=URL_LOAD):
-        super(Mosfilm_Loader, self).__init__(film, host, url_load)
-        self.url_serch = 'search/?{}'.format(urllib.urlencode({'b_search_words': u'Девчата', 'p_f_1_title': u'Девчата', 'p_f_2_title': u'Девчата'}))
+class MosfilmRobot(object):
+    def __init__(self, film_id):
+        self.film = Films.objects.get(pk=film_id)
+        search_film = urllib.urlencode({'sb_search_words': self.film.name.encode('utf-8'), 'p_f_1_title': self.film.name.encode('utf-8'), 'p_f_2_title': self.film.name.encode('utf-8')})
+        self.url_search = 'search/?{}'.format(search_film)
 
-    def get_url(self, load_function):
-        print self.url_serch
+    def get_data(self):
+        url = "http://%s/%s" % (HOST, self.url_search, )
+        content = urllib2.urlopen(url)
+        film_link = parse_search(content, self.film.name.encode('utf-8'))
+        if film_link is None:
+            raise NoSuchFilm(self.film)
+        dict_film = self.get_dict(self.film, film_link)
+        save_location(**dict_film)
+
+
+    def get_dict(self, film, film_link):
+        link = film_link
+        resp_dict = sane_dict(film)
+        resp_dict['value'] = link
+        resp_dict['url_view'] = link
+        resp_dict['price'] = 0
+        return resp_dict
