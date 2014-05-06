c8a30464a81541609bd571bce746e77b5e33a07c
Ivan <wolko_dav@mail.ru>
2014-05-06 19:11:52 +0400

    Check PEP8. Add page_login and page_registration

---
+++ apps/users/api/user_token_auth.py
@@ -0,0 +1,59 @@
+# coding: utf-8
+from rest_framework.authtoken.models import Token
+from rest_framework.views import APIView
+from rest_framework.authtoken import views
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.decorators import permission_classes
+
+from apps.users.models.api_session import UsersApiSessions, SessionToken
+from apps.users.api.utils import create_new_session
+
+
+class ObtainAuthToken(views.ObtainAuthToken):
+
+    def post(self, request, format=None, *args, **kwargs):
+        return super(ObtainAuthToken, self).post(request)
+
+
+class ObtainSessionToken(APIView):
+
+    def get(self, request, format=None, *args, **kwargs):
+        try:
+            session = create_new_session(request.user)
+            response_dict = {
+                'session': session.pk,
+                'expires': session.get_expiration_time(),
+                'session_token': session.token.key,
+            }
+        except Exception as e:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+
+        return Response(response_dict, status=status.HTTP_200_OK)
+
+    @permission_classes((IsAuthenticated, ))
+    def delete(self, request, format=None, *args, **kwargs):
+        token = request.auth
+        try:
+            session = UsersApiSessions.objects.get(token=token.pk)
+            session.active = False
+            session.save()
+        except UsersApiSessions.DoesNotExist as e:
+            return Response({'e': e.message}, status=status.HTTP_400_BAD_REQUEST)
+
+        return Response(status=status.HTTP_200_OK)
+
+
+class RevokeSessionToken(APIView):
+
+    permission_classes = (IsAuthenticated, )
+
+    def get(self, request, format=None, *args, **kwargs):
+        user = request.user
+        session_keys = SessionToken.objects.filter(user=user).values_list('key', flat=True)
+        UsersApiSessions.objects.filter(token__in=session_keys).update(active=False)
+        token = user.auth_token
+        token.delete()
+        Token.objects.create(user=user)
+        return Response(status.HTTP_200_OK)
