71c1625b88406337ae915db3d79bb2a81680ad16
tumani1 <itumanov@aaysm.com>
2014-05-20 12:25:37 +0400

    - дабавил сигналы на genres модель

--- apps/films/signals.py
+++ apps/films/signals.py
@@ -1,11 +1,14 @@
 # coding: utf-8
 
-from django.db.models.signals import post_save, pre_delete
+from django.db.models.signals import post_save, pre_delete, post_delete
 from django.dispatch import receiver
 
-from apps.films.models import Persons
+from apps.films.models import Persons, Genres
 from utils.common import create_thumbnail, delete_thumbnail, check_callable_function
 
+__all__ = [
+    'post_save_handler', 'pre_delete_handler', 'erase_cache_genre_handler'
+]
 
 @receiver(post_save, sender=Persons)
 def post_save_handler(sender, **kwargs):
@@ -17,3 +20,11 @@ def post_save_handler(sender, **kwargs):
 def pre_delete_handler(sender, **kwargs):
     if check_callable_function(kwargs['instance'], 'photo'):
         delete_thumbnail(kwargs['instance'].photo.path)
+
+
+@receiver([post_delete, post_save], sender=Genres)
+def erase_cache_genre_handler(sender, **kwargs):
+    from django.core.cache import cache
+
+    GENRES_CACHE_KEY = 'all_genres'
+    cache.delete(GENRES_CACHE_KEY)
