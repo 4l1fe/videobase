6ec0394084ebf8eca0ba07230b3fcd570bb4f933
tumani1 <itumanov@aaysm.com>
2014-05-20 12:25:37 +0400

    - дабавил сигналы на genres модель

--- apps/films/models/genres.py
+++ apps/films/models/genres.py
@@ -6,11 +6,14 @@ from django.db import models
 #############################################################################################################
 # Модель жанров фильмов/сериалов
 class Genres(models.Model):
-    name         = models.CharField(max_length=255, verbose_name=u'Название жанра')
-    description  = models.TextField(verbose_name=u'Описание жанра')
+    name        = models.CharField(max_length=255, verbose_name=u'Название жанра')
+    description = models.TextField(verbose_name=u'Описание жанра')
 
     def __unicode__(self):
         return u'[{0}] {1}'.format(self.pk, self.name)
+
+    def get_cache_key(self):
+        return u'{0}-{1}-{2}'.format(self._meta.app_label, self._meta.db_table, 'all')
 
     class Meta(object):
         # Имя таблицы в БД
