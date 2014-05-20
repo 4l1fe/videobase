6c90066bc5c0bf0b0c752246e35291992b0c8115
yyyyy <dkrasnov@aaysm.com>
2014-05-20 13:39:28 +0400

    add feed events in films api

--- apps/films/api/films_action_subscribe.py
+++ apps/films/api/films_action_subscribe.py
@@ -1,4 +1,5 @@
 # coding: utf-8
+import json
 
 from rest_framework.views import APIView
 from rest_framework.response import Response
@@ -8,6 +9,8 @@ from rest_framework.permissions import IsAuthenticated
 from apps.films.models import Films, UsersFilms
 from apps.contents.models import Locations
 from apps.films.constants import APP_USERFILM_SUBS_TRUE, APP_USERFILM_SUBS_FALSE, APP_FILM_SERIAL
+from apps.users.models import Feed
+from django.core.exceptions import ObjectDoesNotExist
 
 
 #############################################################################################################
@@ -43,22 +46,24 @@ class ActSubscribeFilmView(APIView):
         if o_film.type != APP_FILM_SERIAL:
             result = Locations.exist_location(film_id)
             if result:
-                return Response({'error': u'Нельзя подписаться на фильм'}, status=status.HTTP_400_BAD_REQUEST)
+                return Response({'error': u'Нельзя подписаться на фильм, потому что он уже появился в кинотеатрах'}, status=status.HTTP_400_BAD_REQUEST)
 
         # Init data
         subscribed = APP_USERFILM_SUBS_TRUE
-        filter = {
+        pair = {
             'user': request.user,
             'film': o_film,
         }
+        obj_val = json.dumps(dict(id=o_film.id, name=o_film.name))
 
         # Устанавливаем подписку
         try:
-            o_subs = UsersFilms(subscribed=subscribed, **filter)
+            o_subs = UsersFilms(subscribed=subscribed, **pair)
             o_subs.save()
+            Feed.objects.create(user=request.user, type='film-s', object=obj_val)
         except Exception as e:
             try:
-                UsersFilms.objects.filter(**filter).update(subscribed=subscribed)
+                UsersFilms.objects.filter(**pair).update(subscribed=subscribed)
             except Exception as e:
                 return Response({'error': e.message}, status=status.HTTP_400_BAD_REQUEST)
 
@@ -82,9 +87,14 @@ class ActSubscribeFilmView(APIView):
             'user': request.user.pk,
             'film': o_film.pk,
         }
+        obj_val = json.dumps(dict(id=o_film.id, name=o_film.name))
 
         # Удалим подписку
         UsersFilms.objects.filter(**filter).update(subscribed=subscribed)
+        try:
+            Feed.objects.get(user=request.user, object=obj_val).delete()
+        except Feed.DoesNotExist as e:  # если записи события вдруг нету, нет смысла что-либо делать.
+            pass
 
         return Response(status=status.HTTP_200_OK)
 
--- apps/users/models/__init__.py
+++ apps/users/models/__init__.py
@@ -5,6 +5,7 @@ from users_profile import UsersProfile
 from users_pics import UsersPics
 from users_logs import UsersLogs
 from users_rels import UsersRels
+from users_feed import Feed
 # from users_requests import UsersRequests
 from users_socials import UsersSocials
 from api_session import UsersApiSessions
