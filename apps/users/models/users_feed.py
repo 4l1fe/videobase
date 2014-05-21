af2bc4156b73c3b96d9dfd03f9cb647760483c3c
Ivan <wolko_dav@mail.ru>
2014-05-21 19:45:39 +0400

    Delete mistake in backend, add constants to feed, add task to celery by notification and templates to email

--- apps/users/models/users_feed.py
+++ apps/users/models/users_feed.py
@@ -2,13 +2,15 @@
 from django.contrib.auth.models import User
 from django.db import models
 
+from apps.users.constants import APP_FEED_TYPE
+
 
 class Feed(models.Model):
     """Содержит имена полей, которые переопределяют питоновские стандартные объекты.
     """
     user = models.ForeignKey(User, verbose_name="Пользователь", null=True, blank=True)
     created = models.DateTimeField(verbose_name="Дата создания", auto_now=True)
-    type = models.CharField(verbose_name="Тип связанного объекта", max_length=255, name='type')
+    type = models.CharField(verbose_name="Тип связанного объекта", choices=APP_FEED_TYPE, max_length=255, name='type')
     object = models.TextField(verbose_name="Связанный объект", name='object')
     text = models.TextField(verbose_name="Текст")
 
--- apps/users/tasks.py
+++ apps/users/tasks.py
@@ -4,10 +4,41 @@ from django.template.loader import render_to_string
 
 from celery.task import task
 
+from apps.films.models import Films, Persons, UsersFilms, UsersPersons
+from apps.users.constants import APP_NOTIFICATION_TEMPLATE,\
+    APP_NOTIFICATION_EMAIL_SUBJECT, FILM_O, PERSON_O
 
-@task(name="confirm_register")
+
+@task(name="confirm_register", queue="send_mail")
 def send_template_mail(subject, tpl_name, context, to):
     tpl = render_to_string(tpl_name, context)
     msg = EmailMultiAlternatives(subject=subject, to=to)
     msg.attach_alternative(tpl, 'text/html')
     msg.send()
+
+
+@task(name="notification", queue="notification")
+def notification(id_, type_):
+    if type_ not in (FILM_O, PERSON_O, ):
+        raise ValueError("Not valid argument")
+    tpl_name = APP_NOTIFICATION_TEMPLATE[type_]
+    subject = APP_NOTIFICATION_EMAIL_SUBJECT[type_]
+    if type_ == FILM_O:
+        model_obj = Films
+        model_user = UsersFilms
+        query = {'person': id_}
+    else:
+        model_obj = Persons
+        model_user = UsersPersons
+        query = {'film': id_}
+
+    obj = model_obj.objects.get(id=id_)
+    context = {'object': obj}
+    to = model_user.objects.filter(**query).exclude(user__email='').values_list("user__email", flat=True)
+    kw = {
+        'subject': subject,
+        'tpl_name': tpl_name,
+        'to': to,
+        'context': context,
+    }
+    send_template_mail.apply_async(kwargs=kw)
