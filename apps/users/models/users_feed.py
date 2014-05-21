eaa2e1115a490cdb8cd22b6f2ea03f0fea79ff9d
tumani1 <itumanov@aaysm.com>
2014-05-21 18:20:11 +0400

    - первая реализация фидов

--- apps/users/models/users_feed.py
+++ apps/users/models/users_feed.py
@@ -1,11 +1,14 @@
-# coding=utf-8
+# coding: utf-8
+
 from django.contrib.auth.models import User
 from django.db import models
 
 
 class Feed(models.Model):
-    """Содержит имена полей, которые переопределяют питоновские стандартные объекты.
     """
+    Содержит имена полей, которые переопределяют питоновские стандартные объекты.
+    """
+        
     user = models.ForeignKey(User, verbose_name="Пользователь", null=True, blank=True)
     created = models.DateTimeField(verbose_name="Дата создания", auto_now=True)
     type = models.CharField(verbose_name="Тип связанного объекта", max_length=255, name='type')
