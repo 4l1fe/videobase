e654950abae0f2118339ff886d34cbd69da2a43f
tumani1 <itumanov@aaysm.com>
2014-05-22 11:35:42 +0400

    - перенес выборку фидов в модель

--- apps/users/api/users_feeds.py
+++ apps/users/api/users_feeds.py
@@ -2,7 +2,6 @@
 
 from rest_framework import status
 from rest_framework.views import APIView
-from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 
 from apps.users.models import Feed
@@ -11,8 +10,6 @@ from apps.users.api.serializers import vbFeedElement
 
 
 class UsersFeedsView(APIView):
-
-    permission_classes = (IsAuthenticated, )
 
     def get(self, request, user_id, format=None, *args, **kwargs):
         pass
--- apps/users/models/users_feed.py
+++ apps/users/models/users_feed.py
@@ -10,15 +10,38 @@ class Feed(models.Model):
     """
     Содержит имена полей, которые переопределяют питоновские стандартные объекты.
     """
-        
+
     user = models.ForeignKey(User, verbose_name="Пользователь", null=True, blank=True)
     created = models.DateTimeField(verbose_name="Дата создания", auto_now=True)
     type = models.CharField(verbose_name="Тип связанного объекта", choices=APP_FEED_TYPE, max_length=255, name='type')
     object = models.TextField(verbose_name="Связанный объект", name='object')
     text = models.TextField(verbose_name="Текст", null=True, blank=True)
 
+
     def __unicode__(self):
         return u"[{id}]{type}".format(id=self.pk, type=self.get_type_display())
+
+
+    @classmethod
+    def get_feeds_by_user(self, user_id, uf=[], up=[], offset=0, limit=20, *args, **kwargs):
+        sql = """
+          SELECT * FROM "users_feed"
+          WHERE ("users_feed"."user_id"=%s OR "users_feed"."user_id" IS NULL) AND (
+            CASE "users_feed"."user_id" IS NULL WHEN
+              CASE
+                WHEN "users_feed"."type"=%s THEN
+                  CAST(coalesce(object->>'id', '0') AS integer) IN %s
+                WHEN "users_feed"."type"=%s THEN
+                  CAST(coalesce(object->>'id', '0') AS integer) IN %s
+                ELSE true END
+              ELSE true
+            END)
+          ORDER BY "users_feed"."created" DESC OFFSET %s LIMIT %s;
+        """
+        params = [user_id,  'film_o', tuple(uf), 'pers_o', tuple(up), offset, limit]
+
+        return self.objects.raw(sql, params)
+
 
     class Meta:
         db_table = 'users_feed'
