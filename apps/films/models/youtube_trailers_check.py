8c1a3de8d0161bee5895a81ade5902e4f7cdf18e
Denis Lyubimov <dlyubimov@aaysm.com>
2014-05-23 18:20:10 +0400

    Fixed missing dependences

---
+++ apps/films/models/youtube_trailers_check.py
@@ -0,0 +1,16 @@
+# coding: utf-8
+from django.db import models
+
+class YoutubeTrailerCheck(models.Model):
+    film = models.ForeignKey('Films', verbose_name=u'Film')
+    last_check = models.DateTimeField(verbose_name=u'Last try datetime')
+    was_successfull = models.BooleanField(verbose_name = 'Was finding a trailer succesfull')
+
+    class Meta(object):
+        # Имя таблицы в БД
+        db_table = 'youtube_trailer_check'
+        app_label = 'films'
+        verbose_name = u'Youtube try'
+        verbose_name_plural = u'Youtube tries'
+
+        
