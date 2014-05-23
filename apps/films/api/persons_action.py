878a9bd4c65c0f09fbacf91a029b59ca93148b97
Krasnov Dmitriy <dkrasnov@aaysm.com>
2014-05-23 18:52:14 +0400

    pre ...

--- apps/films/api/persons_action.py
+++ apps/films/api/persons_action.py
@@ -12,6 +12,16 @@ class PersonActionAPIView(APIView):
 
     permission_classes = (IsAuthenticated, )
 
+    def _update_or_create_feed(self, type_, obj_val):
+        ffeeds = Feed.objects.filter(user=self.request.user, type=type_)
+        feeds = [f for f in ffeeds]
+        objs = [f.object for f in ffeeds]
+        try:
+            f = feeds[objs.index(obj_val)]
+            f.save()
+        except ValueError:
+            Feed.objects.create(user=self.request.user, type=type_, object=obj_val)
+
     def _handle(self, subscribed, request, format=None, resource_id=None):
         try:
             person = Persons.objects.get(id=resource_id)
@@ -23,9 +33,10 @@ class PersonActionAPIView(APIView):
             up, up_created = UsersPersons.objects.get_or_create(**filter_)
             up.subscribed = subscribed
             up.save()
-            f, f_created = Feed.objects.get_or_create(user=request.user, type='pers-s', object=obj_val)
+            self._update_or_create_feed('pers-s', obj_val)
             if not subscribed:
-                f.delete()
+                for f in Feed.objects.filter(user=request.user, type='pers-s'):
+                    if f.object == obj_val: f.delete()
 
             return Response(status=status.HTTP_200_OK)
         except Exception, e:
