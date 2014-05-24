6a2252aaf68028aa4a72fd6d8fe8041104d4f7e9
tumani1 <itumanov@aaysm.com>
2014-05-24 13:16:48 +0400

    - добавил комментарии

--- apps/films/models/youtube_trailers_check.py
+++ apps/films/models/youtube_trailers_check.py
@@ -1,10 +1,13 @@
 # coding: utf-8
+
 from django.db import models
 
+
 class YoutubeTrailerCheck(models.Model):
-    film = models.ForeignKey('Films', verbose_name=u'Film')
-    last_check = models.DateTimeField(verbose_name=u'Last try datetime')
-    was_successfull = models.BooleanField(verbose_name = 'Was finding a trailer succesfull')
+    film            = models.ForeignKey('Films', verbose_name=u'Film')
+    last_check      = models.DateTimeField(verbose_name=u'Last try datetime')
+    was_successfull = models.BooleanField(verbose_name=u'Was finding a trailer succesfull')
+
 
     class Meta(object):
         # Имя таблицы в БД
@@ -12,5 +15,3 @@ class YoutubeTrailerCheck(models.Model):
         app_label = 'films'
         verbose_name = u'Youtube try'
         verbose_name_plural = u'Youtube tries'
-
-        
