edcb101126bda473d9127aacbee59c666ebd2da1
Denis Lyubimov <dlyubimov@aaysm.com>
2014-06-23 15:38:39 +0400

    Changed routing for register template

--- nodejs/zerorpcserver.js
+++ nodejs/zerorpcserver.js
@@ -8,7 +8,7 @@ var renderers = {
     index: function(context) {return jade.renderFile(tmpl_path + 'page_index.jade', context)},
     film: function(context) {return jade.renderFile(tmpl_path + 'page_film.jade', context)},
     person: function(context) {return jade.renderFile(tmpl_path + 'page_person.jade', context)},
-    register: function(context) {return jade.renderFile(tmpl_path + 'page_registration.jade', context)},
+    register: function(context) {return jade.renderFile(tmpl_path + 'page_register.jade', context)},
     user: function(context) {return jade.renderFile(tmpl_path + 'page_user.jade', context)},
     login: function(context) {return jade.renderFile(tmpl_path + 'page_login.jade', context)},
     playlist: function(context) {return jade.renderFile(tmpl_path + 'page_playlist.jade', context)},
