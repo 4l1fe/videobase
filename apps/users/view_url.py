65f6eb5d75e2c245e816886befbad480ad72218f
tumani1 <itumanov@aaysm.com>
2014-06-24 10:59:09 +0400

    - рефактор урлов

---
+++ apps/users/view_url.py
@@ -0,0 +1,17 @@
+# coding: utf-8
+
+from django.conf.urls import patterns, url
+from apps.users import views
+
+urlpatterns = patterns('',
+    url(r'^register/$', views.RegisterUserView.as_view()),
+    url(r'^restore-password/$', views.RestorePasswordView.as_view()),
+    url(r'^login/$', views.LoginUserView.as_view(), name='login_view'),
+    url(r'^user/profile/$', views.UserProfileView.as_view(), name='profile_view'),
+    url(r'^user/(?P<user_id>\d+)/$', views.UserView.as_view()),
+    url(r'^feed/$', 'apps.users.views.feed_view', name='user_feed_view'),
+
+    # Auth
+    url('^tokenize/?$', views.TokenizeView.as_view(), name="tokenize"),
+)
+
