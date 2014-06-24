8ead1182bd0c37317a4be6360a6d5d16ecc03d3b
tumani1 <itumanov@aaysm.com>
2014-06-24 10:35:58 +0400

    - отмена персонализированной выборки на странице поиска

--- apps/films/views.py
+++ apps/films/views.py
@@ -310,7 +310,7 @@ def search_view(request, *args, **kwargs):
 
     if request.REQUEST.get('text'):
         try:
-            resp_dict['films'] = SearchFilmsView.as_view()(request).data
+            resp_dict['films'] = SearchFilmsView.as_view()(request, personalize=False).data
         except Exception, e:
             pass
 
