27990bb6c3178cc0af9bed3373817d94749d9b8e
Ivan <ikrivosheev@aaysm.com>
2014-06-24 12:17:34 +0400

    Add register and login for user

--- apps/users/view_url.py
+++ apps/users/view_url.py
@@ -4,9 +4,9 @@ from django.conf.urls import patterns, url
 from apps.users import views
 
 urlpatterns = patterns('',
-    url(r'^register/$', views.RegisterUserView.as_view()),
+    url(r'^register$', views.RegisterUserView.as_view()),
     url(r'^restore-password/$', views.RestorePasswordView.as_view()),
-    url(r'^login/$', views.LoginUserView.as_view(), name='login_view'),
+    url(r'^login$', views.LoginUserView.as_view(), name='login_view'),
     url(r'^user/profile/$', views.UserProfileView.as_view(), name='profile_view'),
     url(r'^user/(?P<user_id>\d+)/$', views.UserView.as_view()),
     url(r'^feed/$', 'apps.users.views.feed_view', name='user_feed_view'),
