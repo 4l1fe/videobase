94da2d9c1e669be187d2d49cb372505091398d86
Eugene <eshangin@aaysm.com>
2014-06-26 18:16:49 +0400

    Изменил сохранение в Location для роботов с value

--- apps/robots/constants.py
+++ apps/robots/constants.py
@@ -14,6 +14,8 @@ APP_ROBOTS_TRY_NO_SUCH_PAGE = 'pagefail'
 APP_ROBOTS_TRY_PARSE_ERROR = 'parsefail'
 APP_ROBOTS_TRY_SUCCESS = 'success'
 
+APP_ROBOT_VALUE = ['ivi', 'nowru', 'tvigle', 'zoomby', 'tvzavr', 'mosfilm']
+
 APP_ROBOTS_TRY_OUTCOME = (
     (APP_ROBOTS_TRY_SITE_UNAVAILABLE, u'Сайт недоступен'),
     (APP_ROBOTS_TRY_NO_SUCH_PAGE, u'Нет такой страницы'),
