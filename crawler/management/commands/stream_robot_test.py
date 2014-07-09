918b7cb6c6f590e3ec5670e7dde82fbe21c8fda1
sergey.i <sipatov@aaysm.com>
2014-07-09 16:24:12 +0400

    imhonet.ru and dbpedia perser for update persons biography.

--- crawler/management/commands/stream_robot_test.py
+++ crawler/management/commands/stream_robot_test.py
@@ -1,7 +1,11 @@
 from django.core.management.base import BaseCommand
-from crawler.robot_start import launch_next_robot_try
+from crawler.person_info import update_person_info
+from apps.films.models.persons import Persons
 
 
 class Command(BaseCommand):
     def handle(self, *args, **kwargs):
-        launch_next_robot_try('zabava_ru', 11080)
+        pass
+        # for i in range(1, 101):
+        #     person = Persons.objects.get(id=i)
+        #     update_person_info(person)
