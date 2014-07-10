dcf9c5817a9d2cc862674b4bd87d4474a5a529ae
Vladimir Mikryukov <vmikrukov@aaysm.com>
2014-07-10 14:12:10 +0400

    Implemented useless site names checker and corrector for film description

--- data/management/commands/run_test_checker.py
+++ data/management/commands/run_test_checker.py
@@ -8,6 +8,8 @@ import data.film_facts.checker
 class Command(NoArgsCommand):
     def handle_noargs(self, **options):
         f = Films.objects.get(pk=91)
+        f.description = u"4 " + f.description
+        #f.description = f.description + u" NOW.RU  "
         ft = FilmExtras.objects.filter(film=f)
         print f.description
         print data.film_facts.checker.film_checker.check_and_correct(f)
