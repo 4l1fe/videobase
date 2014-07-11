0d1ca8146bcca11c7555457fee4bd60a8dca099e
sergey.i <sipatov@aaysm.com>
2014-07-11 15:45:52 +0400

    change now_ru robot, and added tor_ flag to simple_tor_get

--- crawler/locrobots/now_ru/loader.py
+++ crawler/locrobots/now_ru/loader.py
@@ -3,9 +3,10 @@ from crawler.core.exceptions import NoSuchFilm
 import parsers
 import urllib
 from crawler.core import BaseLoader
+from utils.common import url_with_querystring
 
 HOST = 'www.now.ru'
-URL_SEARCH = 'search?{}'
+URL_SEARCH = 'search'
 URL_LOAD = ''
 
 
@@ -17,8 +18,9 @@ class NOW_Loader(BaseLoader):
 
     def get_url(self, load_function):
         url = "http://%s/%s" % (self.host, self.search_url)
-        response = load_function(url, params=self.params)
-        link = parsers.parse_search(response, self.film.name)
+        url = url_with_querystring(url, **self.params)
+        response = load_function(url)
+        link = parsers.parse_search(response, self.film.name, self.film.release_date.year)
         if link is None:
             raise NoSuchFilm(self.film)
         self.url_load = link
