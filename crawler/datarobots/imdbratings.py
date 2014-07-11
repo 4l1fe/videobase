a69c3cb1fbef9109a391d2aeaeb57c1f55190322
Denis Lyubimov <dlyubimov@aaysm.com>
2014-07-11 13:59:30 +0400

    Merge fix

---
+++ crawler/datarobots/imdbratings.py
@@ -0,0 +1,98 @@
+# coding: utf-8
+import gzip
+import requests
+import StringIO
+from crawler.constants import RATING_GZIP_FILE_URL
+from itertools import dropwhile
+import re
+from apps.films.models import Films
+import HTMLParser
+import logging
+import datetime
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+
+
+regex = re.compile('[ ]+[.0-9]{10}[ ]+(?P<votes>[0-9]+)[ ]{3}(?P<rating>[0-9][.][0-9])[ ](?P<name>.+)')
+
+
+def get_rating_source(debug=False):
+
+    output = StringIO.StringIO()
+    if debug:
+        with open('./crawler/ratings.list.gz') as gf:
+            output.write(gf.read())
+
+    else:
+        data_request = requests.get(RATING_GZIP_FILE_URL)
+        output.write(data_request.content)
+
+    output.seek(0)
+    gzf = gzip.GzipFile(fileobj=output)
+    return gzf
+
+
+def dict_gen(line_iter):
+
+    for i, line in enumerate(line_iter):
+        match = re.match(regex, line.decode('latin-1'))
+        if match:
+            yield match.groupdict()
+
+
+def name_wrapper(dict_list):
+    for filmdict in dict_list:
+        namestring = filmdict['name']
+        name_regex = ('[ ]["](?P<name>[^"]+)["]','[ ](?P<name>[^(]+)')
+        nl = namestring.split()
+        di = dropwhile(lambda n: re.match('[(][0-9]{4}[)]',n) is None,nl[::-1])
+        try:
+            year = next(di)[1:-1]
+            name = next((re.sub('[{][^}]+[}]([ ]|\Z)','',m.groupdict()['name']).strip().lower() for m in (re.match(regex,namestring) for regex in name_regex) if m ))
+            yield ((year, name),filmdict)
+        except StopIteration:
+            logging.debug(u"Couldn't parse name year for {}".format(namestring).encode('utf-8'))
+
+
+def ny_full_dict(debug=False):
+    return dict(name_wrapper(dict_gen(get_rating_source(debug))))
+
+
+def value_dict_update(year, value):
+    value.update({'year': year})
+    return value
+
+
+def process_all():
+    '''
+    Process all films found in our database and update ratings for them
+    if they exist in downloaded file.
+    '''
+    h = HTMLParser.HTMLParser()
+    full_dict = ny_full_dict(False)
+    
+    name_dict = dict((key[1], value_dict_update(key[0], value)) for key, value in full_dict.items())
+
+    
+    changed_ratings = 0
+    fail_years = 0
+    for i, film in enumerate(Films.objects.all()):
+        key = h.unescape(film.name_orig).lower().strip()
+        if len(film.name_orig) < 3:
+            continue
+        
+        if key in name_dict:
+            imdb_date = datetime.datetime.strptime(name_dict[key]['year'], "%Y").date()
+            changed_ratings += 1
+            logger.info((u"Found rating for {} ".format(film.name_orig)).encode("utf-8"))
+            rdict = name_dict[key]
+            logger.debug(("Rating before {} Count before {} ".format(film.rating_imdb, film.rating_imdb_cnt)).encode("utf-8"))
+            
+            film.rating_imdb = 0 if rdict['rating'] is None else rdict['rating']
+            film.rating_imdb_cnt = 0 if rdict['votes'] is None else rdict['votes']
+            logger.debug(("Rating after {} Count after {}".format(film.rating_imdb, film.rating_imdb_cnt)).encode("utf-8"))
+            film.save()
+    
+        print "Films ratings found {}".format(changed_ratings)
+        print "Films overall {}".format(i+1)
---
+++ crawler/datarobots/youtube_com/youtube_trailers.py
@@ -0,0 +1,143 @@
+# coding: utf-8
+'''
+
+Необходимо создать робот поиска трейлеров на youtube.
+
+Поиск по следующему алгоритму:
+
+1. Поиск по Название фильма + "трейлер"
+2. Если в п. 1 ничего не найдено, то Оригинальное название фильма + "трейлер"
+3. Если в п. 2 ничего не найдено, то Оригинальное название фильма + "trailer"
+
+Если трейлер не найден, то повторять поиск для фильмов:
+
+1. Для фильмов с датой выхода больше текущей и фильмов не старше одного года - раз в день
+2. Для фильмов не старше 3 лет - один раз в неделю
+3. Для остальных фильмов - раз в месяц.
+
+'''
+
+
+import gdata.youtube
+import gdata.youtube.service
+from itertools import chain
+from apps.films.models import YoutubeTrailerCheck
+from apps.films.models import FilmExtras
+from apps.films.constants import APP_FILM_TYPE_ADDITIONAL_MATERIAL_TRAILER
+from django.utils import timezone
+
+delays = (
+    #1. Для фильмов с датой выхода больше текущей и фильмов не старше одного года - раз в день
+    (timezone.timedelta(days=365), timezone.timedelta(days =1)),
+    #2. Для фильмов не старше 3 лет - один раз в неделю
+    (timezone.timedelta(days=365*3), timezone.timedelta(days =7)),
+    #3. Для остальных фильмов - раз в месяц.
+    (timezone.timedelta(days=365*3 +1), timezone.timedelta(days =1)),
+)
+
+SEARCH_STRINGS_RU = (u'официальный русский трейлер фильма hd',u'русский трейлер фильма hd',u'русский трейлер HD',u'трейлер на русском',u'официальный трейлер',u'русский трейлер',u'тв-ролик',u'финальный трейлер',u'промо ролик')
+SEARCH_STRINGS_EN =  (u'international trailer hd', u'official trailer hd',u'trailer hd', u'international trailer', u'official teaser', u'trailer')
+FAIL_STRINGS = (u'interview',u'интервью',u'premiere',u'премьера',u'review',u'обзор',u'conference',u'behind the scenes',u'gameplay',u'parody',u'videogame')
+
+
+def query_search(film_name,trailer_word):
+    client = gdata.youtube.service.YouTubeService()
+    query = gdata.youtube.service.YouTubeVideoQuery()
+
+    query.vq = (film_name + trailer_word).encode('utf-8')
+    query.max_results = 25
+    
+
+    feed = client.YouTubeQuery(query)
+
+    for f in feed.entry:
+        
+        title = f.title.text.lower().decode('utf-8')
+        if (film_name.lower() in title) and (trailer_word.lower() in title and sum(s in title for s in FAIL_STRINGS)==0):
+            yield f.title.text, f.link[0].href
+
+
+
+def get_film_trailer(film):
+
+    trailer_name,link = next(
+        chain(
+            chain(
+                *(query_search(film.name,w) for w in SEARCH_STRINGS_RU)),
+            
+        chain(
+            *(query_search(film.name_orig,w) for w in SEARCH_STRINGS_EN))
+        ),
+        None)
+    
+    return trailer_name,link
+        
+def process_film(film):
+
+    try:
+        ytchk= YoutubeTrailerCheck.objects.get(film = film)
+
+    except YoutubeTrailerCheck.DoesNotExist:
+
+        ytchk = YoutubeTrailerCheck(film=film,
+                                    last_check=timezone.now() - timezone.timedelta(days = 1000),
+                                    was_successfull =False
+
+        )
+        ytchk.save()
+
+
+    if ytchk.was_successfull:
+        print u"Trailer already set"
+        return
+    else:
+        for delta,delay in delays:
+            
+            if  timezone.now().date() - film.release_date < delta:
+
+                if ytchk.last_check - timezone.now() < delay:
+
+                    print u"Trying to get trailer for {} from youtube".format(film)
+                    trtuple = get_film_trailer(film)
+                    if trtuple:
+                        trailer_name, link = trtuple
+
+                        fe = FilmExtras(film= film,
+                                    type = APP_FILM_TYPE_ADDITIONAL_MATERIAL_TRAILER,
+                                    name = trailer_name,
+                                    name_orig = trailer_name,
+                                    description = trailer_name,
+                                    url = link )
+
+                        fe.save()
+                        ytchk.was_successfull = True
+                        print u"Succesfully set trailer"
+                    else:
+                        print u"Failed to find trailer"
+                        ytchk.was_successfull = False
+                        
+                    ytchk.last_check = timezone.now()
+                    ytchk.save()
+                return
+                
+
+
+        
+
+
+    
+
+
+
+
+
+
+
+
+    
+
+
+
+    
+    
+    
