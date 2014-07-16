c9c3a0c7ec944c3faaf2728d035e8fbb64e02fe4
tumani1 <itumanov@aaysm.com>
2014-07-16 13:53:29 +0400

    - fixed sorted poster for film

--- apps/films/api/serializers/vb_film.py
+++ apps/films/api/serializers/vb_film.py
@@ -153,7 +153,7 @@ class vbFilm(serializers.ModelSerializer):
     # ---------------------------------------------------------------------------------------
     def _rebuild_location(self):
         locations = Locations.objects.filter(content__film__in=self.list_obj_pk)\
-            .order_by('content__film').select_related('content')
+            .order_by('content__film', 'content__id').select_related('content')
 
         result = {}
         for item in locations:
