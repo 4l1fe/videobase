2b8bead497ff7998543994697612ea87bbd3cac2
Vladimir Mikryukov <vmikrukov@aaysm.com>
2014-07-17 12:05:00 +0400

    Implemented film trailer title checker function

--- data/management/commands/run_test_checker.py
+++ data/management/commands/run_test_checker.py
@@ -7,10 +7,11 @@ import data.film_facts.checker
 
 class Command(NoArgsCommand):
     def handle_noargs(self, **options):
-        f = Films.objects.get(pk=12)
+        f = Films.objects.get(id=799)
+        #Les Lapins Crétins : Retour vers le passé - Trailer HD
         #f.description = u"4 " + f.description
         #f.description = f.description + u" NOW.RU  "
         ft = FilmExtras.objects.filter(film=f)
-        print f.countries.all()
+        print f.name
         print data.film_facts.checker.film_checker.check_and_correct(f)
-        print f.countries.all()
+        print f.name
