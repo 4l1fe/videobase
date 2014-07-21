fc66b28203d68376b0479bb145da30834eaae05c
tumani1 <itumanov@aaysm.com>
2014-07-21 13:35:23 +0400

    - fixed requirements by feed v2.4

---
+++ apps/rss/urls.py
@@ -0,0 +1,11 @@
+# coding: utf-8
+
+from django.conf.urls import patterns, url
+
+
+urlpatterns = patterns('apps.rss.views',
+    url(r'^feed_tw/$', 'get_feed_tw'),
+    url(r'^feed_vk/$', 'get_feed_vk'),
+    url(r'^feed/$', 'get_feed'),
+    url(r'^feed_fb/$', 'get_feed_fb')
+)
