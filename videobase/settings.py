c76ea4077f59eec713c510ff127ad02319f87b29
Ivan <ikrivosheev@aaysm.com>
2014-07-28 10:57:44 +0400

    Добавлено выставление времени в кукках при авторизации

--- videobase/settings.py
+++ videobase/settings.py
@@ -198,7 +198,7 @@ REST_FRAMEWORK = {
 SESSION_SERIALIZER = 'django.contrib.sessions.serializers.PickleSerializer'
 
 LOGIN_URL = '/login'
-SOCIAL_AUTH_LOGIN_REDIRECT_URL = '/tokenize'
+SOCIAL_AUTH_LOGIN_REDIRECT_URL = '/tokenize/'
 LOGIN_ERROR_URL = '/'
 
 ###########################################################
