86afb6648ef2944bdd2d1bf53968ed66fadff0ce
Vladimir Mikryukov <vmikrukov@aaysm.com>
2014-08-05 10:41:49 +0400

    Реализован checker и corrector для постеров у фильма и персон

--- data/management/commands/run_test_checker.py
+++ data/management/commands/run_test_checker.py
@@ -8,8 +8,8 @@ import data.person_facts.checker
 
 class Command(NoArgsCommand):
     def handle_noargs(self, **options):
-        #f = Films.objects.get(id=61)
-        person = Persons.objects.get(pk=1114)
+        f = Films.objects.get(id=33665)
+        #person = Persons.objects.get(kinopoisk_id=23344 ) #1735
         #Les Lapins Crétins : Retour vers le passé - Trailer HD
         #f.description = u"4 " + f.description
         #f.description = f.description + u" NOW.RU  "
@@ -17,7 +17,8 @@ class Command(NoArgsCommand):
         #print f.name
         #print data.film_facts.checker.film_checker.check_and_correct(f)
         #print f.name
-        person.kinopoisk_id = 1554040
-        person.save()
-        print(data.person_facts.checker.person_checker.check_and_correct(person))
-        print(person.name, person.photo, person.bio, person.kinopoisk_id)
+        #person.kinopoisk_id = 1554040
+        #person.save()
+        print(data.film_facts.checker.film_checker.check_and_correct(f))
+        #print(data.person_facts.checker.person_checker.check_and_correct(person))
+        #print(person.name, person.photo.size, person.bio, person.kinopoisk_id)
