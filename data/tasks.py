e7cdffb838ce376134898bbacd46b80f1b4ab0d4
Denis Lyubimov <dlyubimov@aaysm.com>
2014-08-18 17:27:40 +0400

    Rewriting consolidate rating as celery task

--- data/tasks.py
+++ data/tasks.py
@@ -1,9 +1,16 @@
 from data.sitemap import refresh
 from videobase.celery import app
-
+from apps.films.management.commands.consolidate_rating import Command
 
 @app.task(name='refresh_sitemap')
 def refresh_sitemap_task():
 
     refresh()
+
+
+@app.task(name='consolidate_rating')
+def consolidate_rating_task():
+
+    c = Command()
+    c.handle()
 
