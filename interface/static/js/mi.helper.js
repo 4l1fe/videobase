8d3918173df2e9ccfd56379f45548e9d8a416ccf
Konstantin Timkhin <kt@aaysm.com>
2014-08-26 18:43:23 +0400

    - fixed session - added comments on main page - fixed comments in feed

--- interface/static/js/mi.helper.js
+++ interface/static/js/mi.helper.js
@@ -210,7 +210,7 @@ jQuery.fn.visibilityToggle = function() {
             return (document.cookie = [
                 encode(key), '=', stringifyCookieValue(value),
                 options.expires ? '; expires=' + options.expires.toUTCString() : '', // use expires attribute, max-age is not supported by IE
-                options.path    ? '; path=' + options.path : '',
+                options.path    ? '; path=' + options.path : '; path=/',
                 options.domain  ? '; domain=' + options.domain : '',
                 options.secure  ? '; secure' : ''
             ].join(''));
