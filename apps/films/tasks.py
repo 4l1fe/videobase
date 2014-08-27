9c173bf6d16deb8abe49482f0d29b58a63188b40
tumani1 <itumanov@aaysm.com>
2014-08-27 11:33:38 +0400

    - deleted from signals additional save profile - changed queryset and logic by friends feed in the day notification

--- apps/films/tasks.py
+++ apps/films/tasks.py
@@ -10,7 +10,7 @@ from apps.users.models import User, Feed, UsersRels
 from apps.users.tasks import send_template_mail
 from apps.users.api.serializers import vbFeedElement
 from apps.users.constants import APP_USERPROFILE_NOTIFICATION_WEEK, APP_USERPROFILE_NOTIFICATION_DAY, \
-    FILM_NEWSLETTER
+    FILM_RATE, FILM_SUBSCRIBE, FILM_COMMENT
 
 
 @app.task(name="week_newsletter", queue="week_newsletter")
@@ -64,7 +64,7 @@ def personal_newsletter():
         email__isnull=False,
         profile__ntf_frequency=APP_USERPROFILE_NOTIFICATION_DAY,
         profile__confirm_email=True,
-    )
+    ).prefetch_related('profile__user')
 
     for item in o_users:
         # Init data
@@ -76,8 +76,21 @@ def personal_newsletter():
 
         # Проверка длинны
         if len(user_friends):
-            feeds = Feed.objects.filter(user__in=user_friends, type__in=FILM_NEWSLETTER)
-            feeds = vbFeedElement(feeds, many=True).data
+            # Собираем типы фидов из профиля
+            type_film_feed = []
+
+            if item.profile.ntf_frnd_rate:
+                type_film_feed.append(FILM_RATE)
+
+            if item.profile.ntf_frnd_comment:
+                type_film_feed.append(FILM_COMMENT)
+
+            if item.profile.ntf_frnd_subscribe:
+                type_film_feed.append(FILM_SUBSCRIBE)
+
+            if len(type_film_feed):
+                feeds = Feed.objects.filter(user__in=user_friends, type__in=type_film_feed)
+                feeds = vbFeedElement(feeds, many=True).data
 
         # Выборка фильмов
         films = Films.objects.filter(uf_films_rel__user=item.id, uf_films_rel__subscribed=APP_USERFILM_SUBS_TRUE)
