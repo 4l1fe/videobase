9d05ffc267d868924383bcea9795b7cc9a71bb3f
Denis Lyubimov <dlyubimov@aaysm.com>
2014-08-28 11:07:46 +0400

    Fixing Sentry support

--- videobase/settings.py
+++ videobase/settings.py
@@ -251,14 +251,6 @@ SOCIAL_AUTH_PIPELINE = (
 # In minutes
 API_SESSION_EXPIRATION_TIME = 15
 
-if not DEBUG:
-    INSTALLED_APPS += (
-        'raven.contrib.django.raven_compat',  # may be delete later
-    )
-
-    RAVEN_CONFIG = {
-        'dsn': 'http://8684bf8b497047d9ac170fd16aefc873:41e89f4666b24f998125370f3d1a1789@sentry.aaysm.com/2'
-    }
 
 ###########################################################
 CELERYBEAT_SCHEDULE = {
@@ -389,3 +381,13 @@ POSTER_URL_PREFIX = '_260x360'
 TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'
 
 from local_settings import *
+
+if not DEBUG:
+    INSTALLED_APPS += (
+        'raven.contrib.django.raven_compat',  # may be delete later
+    )
+
+    RAVEN_CONFIG = {
+        'dsn': 'http://8684bf8b497047d9ac170fd16aefc873:41e89f4666b24f998125370f3d1a1789@sentry.aaysm.com/2'
+    }
+    
