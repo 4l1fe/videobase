3f66187ad0b0d954f314f12eeaceac52a747846a
Vladimir Mikryukov <vmikrukov@aaysm.com>
2014-09-01 13:20:42 +0400

    Added admin table for robots mail list

--- apps/robots/models/__init__.py
+++ apps/robots/models/__init__.py
@@ -4,3 +4,4 @@ from robots_logs import RobotsLog
 from kinopoisk import KinopoiskTries
 from robots_tries import RobotsTries
 from robots_logs import RobotsInfoLogging
+from robots_mail_list import RobotsMailList
