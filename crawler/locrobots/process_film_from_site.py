c46816d5cb3e50e9300f1177ef045b073caebfff
Vladimir Mikryukov <vmikrukov@aaysm.com>
2014-09-04 12:35:37 +0400

    Fixed some bugs in page dumping

--- crawler/locrobots/process_film_from_site.py
+++ crawler/locrobots/process_film_from_site.py
@@ -5,9 +5,11 @@ from crawler.locrobots.process_films_tasks import *
 __author__ = 'vladimir'
 
 
-def get_html_str_for_file_name(file_name):
-    f = open(file_name,'r')
-    html = f.read()
+def get_html_json_for_file_name(file_name):
+    f = open(file_name, 'r')
+    print file_name
+    raw_text = f.read()
+    html = json.loads(raw_text)
     f.close()
     return html
 
@@ -15,7 +17,7 @@ def get_html_str_for_file_name(file_name):
 def process_film_on_site(site, film_id):
     result = load_film_page_from_site.apply_async((site, film_id))
     html_file_name = result.get(propagate=False)
-    html = get_html_str_for_file_name(html_file_name)
-    return process_one_film(site, film_id, html)
+    html_json = get_html_json_for_file_name(html_file_name)
+    return process_one_film(site, film_id, html_json)
 
 
