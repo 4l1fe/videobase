1beb2dc4f7c0cdda1ee796bc53c34b51a4edb097
tumani1 <itumanov@aaysm.com>
2014-09-09 16:45:26 +0400

    - fixed tests

--- apps/casts/models/__init__.py
+++ apps/casts/models/__init__.py
@@ -1,3 +1,4 @@
+# coding: utf-8
 
 from apps.casts.models.abstract_casts_tags import AbstractCastsTags
 from apps.casts.models.casts import Casts
@@ -10,4 +11,7 @@ from apps.casts.models.extras_casts import CastsExtras
 from apps.casts.models.users_casts import UsersCasts
 
 
-__all__ = ['AbstractCastsTags', 'Casts', 'CastsChats', 'CastsChatsUsers', 'CastsChatsMsgs', 'CastsLocations', 'CastsServices', 'CastsExtras', 'UsersCasts']
+__all__ = [
+    'AbstractCastsTags', 'Casts', 'CastsChats', 'CastsChatsUsers', 'CastsChatsMsgs',
+    'CastsLocations', 'CastsServices', 'CastsExtras', 'UsersCasts'
+]
