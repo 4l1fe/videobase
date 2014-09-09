7cc9162fa01607d7931ea58f9bad4ef513eb3523
sergey.i <sipatov@aaysm.com>
2014-09-09 16:29:20 +0400

    трансляции с live.russia.tv

---
+++ crawler/translation_robot/live_russia_tv/translation_live_russia_tv.py
@@ -0,0 +1,87 @@
+# coding: utf-8
+
+""" Поиск трансляций live.russia.tv """
+
+import re
+from bs4 import BeautifulSoup
+from django.utils import timezone
+
+from crawler.tor import simple_tor_get_page
+
+TRANSLATION_URL = 'http://live.russia.tv'
+PLAYER_LINK = 'http://player.rutv.ru/iframe/live/id/%s/showZoomBtn/false/isPlay/true/'
+MONTHS = {
+    u'января': 1,
+    u'февраля': 2,
+    u'марта': 3,
+    u'апреля': 4,
+    u'мая': 5,
+    u'июня': 6,
+    u'июля': 7,
+    u'августа': 8,
+    u'сентября': 9,
+    u'октября': 10,
+    u'ноября': 11,
+    u'декабря': 12,
+}
+
+
+def parse_translation_live_russia_tv():
+    translation_list = []
+
+    # Get current date
+    current_date = timezone.now()
+
+    # Get page
+    translation_page = simple_tor_get_page(TRANSLATION_URL)
+    soup = BeautifulSoup(translation_page)
+
+    translation_bloc = soup.find('div', {'class': ['broadcasts', 'tab-non-active', 'tab-broadcasts']})
+
+    for trans in translation_bloc.find_all('li'):
+        try:
+
+            # Get date and time
+            time_tag = trans.find('div', {'class': 'time'})
+            time = re.findall(ur'\d+', time_tag.text)
+            date_tag = trans.find('div', {'class': 'label'})
+            date_str = date_tag.text.lower().strip().split()
+
+            if len(date_str) == 1:
+                if date_str[0] == u'сегодня':
+                    date = current_date.replace(hour=int(time[0]), minute=int(time[1]))
+            elif len(date_str) == 3:
+                date = timezone.datetime(year=int(date_str[2]), month=MONTHS[date_str[1]], day=int(date_str[0]),
+                                         hour=int(time[0]), minute=int(time[1]))
+            else:
+                continue
+
+            # Get title
+            title_tag = trans.h2
+            title = title_tag.text
+
+            #Get link
+            href = trans.a.get('href')
+            link = TRANSLATION_URL + href
+
+            #Get video id
+            video_id = href.split('/')[-1]
+
+            #Create dict with information about translation
+            translation_data = {
+                'title': title,
+                'date': date,
+                'price': float(0),
+                'link': link,
+                'meta': None,
+                'embed_code': None,
+                'video_id': video_id,
+                'player': PLAYER_LINK % video_id
+            }
+
+            translation_list.append(translation_data)
+        except Exception, e:
+            print e.message
+
+    return translation_list
+
