91bc845602aa5a36b4dd1769dcdd66142dd6c24e
dmitriy krasnovm <dkrasnov@aaysm.com>
2014-09-11 17:18:02 +0400

    добавлена утилита isiterable(), обновлены методы получения токенов, сериализатор заменяется на предвыборки

--- apps/users/api/serializers/vb_feedelement.py
+++ apps/users/api/serializers/vb_feedelement.py
@@ -1,4 +1,8 @@
 # coding: utf-8
+import os, sys
+sys.path.append('/home/dmitriy/projects/videobase/')
+os.environ['DJANGO_SETTINGS_MODULE'] = 'videobase.settings'
+
 from django.contrib.auth.models import User
 from rest_framework import serializers
 from apps.contents.models import Comments, Locations
@@ -11,53 +15,79 @@ from apps.users.constants import CACHED_FEED_TIMEOUT
 from apps.users.constants import (FILM_RATE, FILM_SUBSCRIBE, FILM_NOTWATCH,
                                   FILM_COMMENT, FILM_O, PERSON_SUBSCRIBE,
                                   PERSON_O, USER_ASK, USER_FRIENDSHIP, SYS_ALL)
+from django.db.models.query import QuerySet
+from utils.common import isiterable
 
 
 class vbFeedElement(serializers.ModelSerializer):
-    user = serializers.SerializerMethodField('get_vbUser')
-    object = serializers.SerializerMethodField('get_object')
+    user = serializers.SerializerMethodField('get_user_field')
+    object = serializers.SerializerMethodField('get_object_field')
 
     def __init__(self, *args, **kwargs):
         super(vbFeedElement, self).__init__(*args, **kwargs)
+        if not (isinstance(self.object, QuerySet) or isinstance(self.object, Feed)):
+            raise TypeError('object must be QuerySet or Feed instance')
+
+        if not isiterable(self.object):
+            self.object = [self.object]
+
         self.user_data = {}  # собираем заранее данные по различным пользователям
         for ins in self.object:
             if ins.user_id and ins.user_id not in self.user_data:
                 self.user_data.update({ins.user_id: vbUser(ins.user).data})
 
-    def get_object(self, obj):
+        films_id = []
+        for feed in self.object:
+            if feed.type in [FILM_RATE, FILM_SUBSCRIBE, FILM_NOTWATCH, FILM_O]:
+                films_id.append(feed.obj_id)
+            if feed.type in [FILM_COMMENT, PERSON_O]:
+                films_id.append(feed.child_obj_id)
+
+        self.films = Films.objects.filter(pk__in=films_id)
+
+        users_films = UsersFilms.objects.filter(user_id__in=self.user_data.keys(),  # собираем рейтинги фильмов
+                                                     film_id__in=films_id).values('user_id', 'film_id', 'rating')
+        self.users_films = {(d['user_id'], d['film_id']): d['rating'] for d in users_films}
+
+    def get_film(self, id_):
+        for film in self.films:
+            if film.id == id_:
+                return film
+
+    def get_object_field(self, obj):
         user_id = obj.user_id if obj.user_id else ''  # может придти None. Не во всех событиях указывается пользователь
         key = '{},{},{}'.format(user_id, obj.type, obj.obj_id)
         object_ = cache.get(key)
         if not object_:
             try:
                 if obj.type == FILM_RATE:
-                    film = Films.objects.get(pk=obj.obj_id)
-                    rating = UsersFilms.objects.get(user_id=obj.user_id, film=film).rating
+                    film = self.get_film(obj.obj_id)
+                    rating = self.users_films[(obj.user_id, obj.obj_id)]
                     object_ = {'id': film.id,
-                              'name': film.name,
-                              'rating': rating}
+                               'name': film.name,
+                               'rating': rating}
                 elif obj.type == FILM_SUBSCRIBE:
-                    film = Films.objects.get(pk=obj.obj_id)
+                    film = self.get_film(obj.obj_id)
                     poster = FilmExtras.get_poster_by_film(film.fe_film_rel.all())
                     object_ = {'id': film.id,
-                              'name': film.name,
-                              'poster': poster,
-                              'description': film.description}
+                               'name': film.name,
+                               'poster': poster,
+                               'description': film.description}
                 elif obj.type == FILM_NOTWATCH:
-                    film = Films.objects.get(pk=obj.obj_id)
+                    film = self.get_film(obj.obj_id)
                     poster = FilmExtras.get_poster_by_film(film.fe_film_rel.all())
                     object_ = {'id': film.id,
-                              'name': film.name,
-                              'poster': poster}
+                               'name': film.name,
+                               'poster': poster}
                 elif obj.type == FILM_COMMENT:
                     comment = Comments.objects.get(pk=obj.obj_id)
-                    film = Films.objects.get(pk=obj.child_obj_id)
+                    film = self.get_film(obj.child_obj_id)
                     object_ = {'id': comment.id,
                                'text': comment.text,
                                'film': {'id': film.id,
                                         'name': film.name}}
                 elif obj.obj_id == FILM_O:
-                    film = Films.objects.get(pk=obj.obj_id)
+                    film = self.get_film(obj.obj_id)
                     poster = FilmExtras.get_poster_by_film(film.fe_film_rel.all())
                     location = Locations.objects.get(pk=obj.child_obj_id)
                     object_ = {'id': film.id,
@@ -74,7 +104,7 @@ class vbFeedElement(serializers.ModelSerializer):
                                'photo': person.get_path_to_photo}
                 elif obj.type == PERSON_O:
                     person = Persons.objects.get(pk=obj.obj_id)
-                    film = Films.objects.get(pk=obj.child_obj_id)
+                    film = self.get_film(obj.child_obj_id)
                     pf = PersonsFilms.objects.filter(person=person, film=film).first()  #TODO: брать роль по статистике
                     object_ = {'id': person.id,
                                'name': person.name,
@@ -111,9 +141,21 @@ class vbFeedElement(serializers.ModelSerializer):
 
         return object_
 
-    def get_vbUser(self, obj):
+    def get_user_field(self, obj):
         return self.user_data.get(obj.user_id, {})
 
     class Meta:
         model = Feed
         fields = ('user', 'created', 'type', 'object', 'text')
+
+
+if __name__ == '__main__':
+    from datetime import datetime
+    cache.clear()
+    start = datetime.now()
+    data = vbFeedElement(Feed.objects.filter(user_id=1), many=True).data
+    end = datetime.now()
+    delta = end - start
+    sec = delta.seconds
+    ms = delta.microseconds/1000
+    print('taken time: {}.{} sec'.format(sec, ms))
--- utils/tokens.py
+++ utils/tokens.py
@@ -5,8 +5,6 @@
 
 import sys
 import json
-from pprint import pprint as pp
-import httplib
 
 python_v = sys.version_info[0]
 
@@ -19,16 +17,15 @@ elif python_v == 3:
     from urllib.parse import urlencode, urljoin
 
 
-HOST = 'dmitriy-book.com'
-SCHEMA_HOST = 'http://{}'.format(HOST)
-# HOST = 'http://127.0.0.1:9000'
+HOST = 'localvsevi'
+ADDR = 'http://{}'.format(HOST)
 
 
-def get_main_token(username='nana@nana.na', password='nana', host=SCHEMA_HOST):
+def get_main_token(username, password, address=ADDR):
     data = urlencode(dict(username=username, password=password))
     if python_v == 3:
         data = data.encode()
-    req = Request(urljoin(host, 'api/v1/auth/login.json'), data)
+    req = Request(urljoin(address, 'api/v1/auth/login.json'), data)
     resp = urlopen(req)
     resp_data = resp.read()
     if python_v == 3:
@@ -37,8 +34,8 @@ def get_main_token(username='nana@nana.na', password='nana', host=SCHEMA_HOST):
     return json_resp['X-MI-TOKEN']
 
 
-def get_session_token(main_token, host=SCHEMA_HOST):
-    req = Request(urljoin(host, 'api/v1/auth/session.json'))
+def get_session_token(main_token, address=ADDR):
+    req = Request(urljoin(address, 'api/v1/auth/session.json'))
     req.add_header('X-MI-TOKEN', main_token)
     resp = urlopen(req)
     resp_data = resp.read()
@@ -48,146 +45,17 @@ def get_session_token(main_token, host=SCHEMA_HOST):
     return json_resp['session_token']
 
 
-def get_films_persons(sessiom_token, id, host=SCHEMA_HOST):
-    req = Request(urljoin(host, 'api/v1/films/{}/persons.json'.format(id)))
-    req.add_header('Authorization', 'X-VB-Token ' + sessiom_token)
+def get_feed(session_token, address=ADDR):
+    req = Request(urljoin(address, 'api/v1/users/2/feed.json?page=2,per_page=1000'))
     resp = urlopen(req)
     resp_data = resp.read()
-    if python_v == 3:
-        resp_data = resp_data.decode()
     json_resp = json.loads(resp_data)
     return json_resp
-
-
-def get_users_persons(session_token, id, page=1, per_page=10, type_='all', host=SCHEMA_HOST):
-    url = urljoin(host, 'api/v1/users/{}/persons.json'.format(id))
-    data = urlencode(dict(page=page, per_page=per_page, type=type_))
-    req = Request(url, data)
-    req.add_header('Authorization', 'X-VB-Token ' + session_token)
-    resp = urlopen(req)
-    resp_data = resp.read()
-    if python_v == 3:
-        resp_data = resp_data.decode()
-    json_resp = json.loads(resp_data)
-    return json_resp
-
-
-def get_person(session_token, id_, meth, extend=False, host=SCHEMA_HOST):
-    req = Request(urljoin(host, 'api/v1/persons/{}.json'.format(id_)))
-    if meth.lower() == 'post':
-        data = urlencode(dict(extend=extend))
-        req.add_data(data)
-    req.add_header('Authorization', 'X-VB-Token ' + session_token)
-    resp = urlopen(req)
-    resp_data = resp.read()
-    if python_v == 3:
-        resp_data = resp_data.decode()
-    json_resp = json.loads(resp_data)
-    return json_resp
-
-
-def get_person_films(id_, meth, page=1, per_page=12, type_='a', host=SCHEMA_HOST):
-    data = urlencode(dict(page=page, per_page=per_page, type_=type_))
-    if meth.lower() == 'post':
-        req = Request(urljoin(host, 'api/v1/persons/{}/filmography.json'.format(id_)))
-        req.add_data(data)
-    else:
-        req = Request(urljoin(host, 'api/v1/persons/{}/filmography.json?'.format(id_)))
-
-    # req.add_header('Authorization', 'X-VB-Token ' + session_token)
-    resp = urlopen(req)
-    resp_data = resp.read()
-    if python_v == 3:
-        resp_data = resp_data.decode()
-    json_resp = json.loads(resp_data)
-    return json_resp
-
-
-def film_subscribe(session_token, method, id_, host=HOST):
-    cl = httplib.HTTPConnection(host)
-    headers = {'X-MI-SESSION': session_token}
-    cl.request(method, '/api/v1/films/{}/action/subscribe.json'.format(id_), headers=headers)
-    resp = cl.getresponse()
-    return resp.read()
-
-
-def film_notwatch(session_token, method, id_, host=HOST):
-    cl = httplib.HTTPConnection(host)
-    headers = {'X-MI-SESSION': session_token}
-    cl.request(method, '/api/v1/films/{}/action/notwatch.json'.format(id_), headers=headers)
-    resp = cl.getresponse()
-    return resp.read()
-
-
-def person_subscribe(session_token, method, id_, host=HOST):
-    cl = httplib.HTTPConnection(host)
-    headers = {'X-MI-SESSION': session_token}
-    cl.request(method, '/api/v1/persons/{}/action/subscribe.json'.format(id_), headers=headers)
-    resp = cl.getresponse()
-    return resp.read()
-
-
-def film_comment(session_token, id_, host=SCHEMA_HOST):
-    headers = {'X-MI-SESSION': session_token}
-    data = urlencode({'text': 'some comment from utils'})
-    req = Request(urljoin(host, 'api/v1/films/{}/action/comment.json'.format(id_)), data=data, headers=headers)
-    resp = urlopen(req)
-    return resp.read()
-
-
-def film_rate(session_token, method, id_, rating=None, host=HOST):
-    cl = httplib.HTTPConnection(host)
-    data = urlencode(dict(rating=rating)) if rating else None
-    headers = {'X-MI-SESSION': session_token, 'Content-type': 'application/x-www-form-urlencoded', 'Accept': 'application/json'}
-    cl.request(method, '/api/v1/films/{}/action/rate.json'.format(id_), headers=headers, body=data)
-    resp = cl.getresponse()
-    return resp.read()
-
-
-def user_friendship(session_token, method, id_, host=HOST):
-    cl = httplib.HTTPConnection(host)
-    headers = {'X-MI-SESSION': session_token}
-    cl.request(method, '/api/v1/users/{}/friendship.json'.format(id_), headers=headers)
-    resp = cl.getresponse()
-    return resp.read()
-
 
 if __name__ == '__main__':
-    mt = get_main_token(username='nana@nana.na', password='nana')
+    mt = get_main_token(username='ak@aaysm.com', password='akka')
     print(mt)
     st = get_session_token(mt)
     print(st)
-    # resp = get_films_persons(st, 3)
-    # pp(resp)
-    # resp = get_users_persons(st, 1, 1, 10)
-    # pp(resp)
-    # resp = get_person(st, 12, 'get')
-    # pp(resp)
-    # resp = get_person(st, 12, 'post')
-    # pp(resp)
-    # resp = get_person(st, 12, 'post', extend=True)
-    # pp(resp)
-    # resp = get_person_films(2, 'get')
-    # pp(resp)
-    # resp = film_subscribe(st, 'PUT', 71)
-    # pp(resp)
-    # resp = film_subscribe(st, 'DELETE', 71)
-    # pp(resp)
-    # resp = film_notwatch(st, 'DELETE', 3998)
-    # pp(resp)
-    # resp = film_notwatch(st, 'PUT', 3998)
-    # pp(resp)
-    # resp = film_rate(st, 'PUT', 2018, 7.7)
-    # pp(resp)
-    resp = film_rate(st, 'DELETE', 2018)
-    pp(resp)
-    # resp = person_subscribe(st, 'DELETE', 6509)
-    # pp(resp)
-    # resp = person_subscribe(st, 'PUT', 6509)
-    # pp(resp)
-    # resp = film_comment(st, 15155)
-    # pp(resp)
-    # resp = user_friendship(st, 'DELETE', 8)
-    # pp(resp)
-    # resp = user_friendship(st, 'GET', 8)
-    # pp(resp)
+    feed = get_feed(st)
+    print(feed)
