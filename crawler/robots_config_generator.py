69e69fb82d692f9e7873f7d28a78ec059eac15da
Vladimir Mikryukov <vmikrukov@aaysm.com>
2014-09-12 13:54:37 +0400

    Fixed  all spaces in robots_config file

--- crawler/robots_config_generator.py
+++ crawler/robots_config_generator.py
@@ -10,16 +10,15 @@ def create_queue_str(robot_name):
 
 def generate_process_section_with_parameters(programm_name, command, log_file_name):
     template = '''[program:{}]
-    command={}
-    process_name=%(program_name)s ; process_name expr (default %(program_name)s)
-    numprocs=1 ; number of processes copies to start (def 1)
-    directory=/var/www/videobase/ ; directory to cwd to before exec (def no cwd)
-    umask=022 ; umask for process (default None)
-    startretries=1 ; max # of serial start failures (default 3)
-    user=www-data ; setuid to this UNIX account to run the program
-    redirect_stderr=true ; redirect proc stderr to stdout (default false)
-    stdout_logfile=/var/log/{}.log
-    '''.format(programm_name, command, log_file_name)
+command={}
+process_name=%(program_name)s ; process_name expr (default %(program_name)s)
+numprocs=1 ; number of processes copies to start (def 1)
+directory=/var/www/videobase/ ; directory to cwd to before exec (def no cwd)
+umask=022 ; umask for process (default None)
+startretries=1 ; max # of serial start failures (default 3)
+user=www-data ; setuid to this UNIX account to run the program
+redirect_stderr=true ; redirect proc stderr to stdout (default false)
+stdout_logfile=/var/log/{}.log'''.format(programm_name, command, log_file_name)
     return template
 
 
