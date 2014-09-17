e9e2b44fe22ba6c09e2593b0d853706757785fdb
Vladimir Mikryukov <vmikrukov@aaysm.com>
2014-09-17 16:16:01 +0400

    Fixed cycle import in loctobots

--- crawler/locrobots/process_film_from_site.py
+++ crawler/locrobots/process_film_from_site.py
@@ -1,8 +1,8 @@
 # coding: utf-8
 import json
 
-from crawler.tasks.locrobots_tasks import load_film_page_from_site
 from crawler.locrobots.process_films_tasks import process_one_film
+from crawler.locrobots.load_film_page import load_film_page_from_site
 
 def get_html_json_for_file_name(file_name):
     html=''
@@ -18,7 +18,7 @@ def get_html_json_for_file_name(file_name):
 
 def process_film_on_site(site, film_id):
     result = load_film_page_from_site.run(site, film_id)
-    # html_file_name = result.get(propagate=False)
-    html_json = get_html_json_for_file_name(result)
+    html_file_name = result.get(propagate=False)
+    html_json = get_html_json_for_file_name(html_file_name)
     return process_one_film(site, film_id, html_json)
 
