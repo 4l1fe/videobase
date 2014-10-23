2d3effc4a0b05da03cde3744331cdc30f21be79e
Denis Lyubimov <dlyubimov@aaysm.com>
2014-10-23 19:54:42 +0400

    Bug with castchats msgs get

--- apps/casts/api/serializers/vbcastchatsmsg.py
+++ apps/casts/api/serializers/vbcastchatsmsg.py
@@ -2,20 +2,26 @@
 
 from rest_framework import serializers
 
-from apps.casts.models import Casts, UsersCasts
+from apps.casts.models import Casts, UsersCasts, CastsChatsMsgs
 from apps.users.api.serializers import vbUser
 
 class vbCastChatMsg(serializers.ModelSerializer):
 
     user = serializers.SerializerMethodField('calc_user')
+    cast_id = serializers.SerializerMethodField('calc_cast_id')
+
+    def calc_cast_id(self, obj):
+
+        return obj.cast.id
 
     def calc_user(self, obj):
         return vbUser(obj.user)
 
+    
     class Meta:
-        model = Casts
+        model = CastsChatsMsgs
         fields = (
-            'id', 'cast_id', 'created', 'text'
+            'id', 'created', 'text'
         )
 
 
