d7da045c171531c2b8014f98100a2e7b8a4835fb
Dmitriy Krasnov <dkrasnov@aaysm.com>
2014-10-24 17:53:08 +0400

    расширена команда, расширена генерация

--- crawler/management/commands/generate_robots_config.py
+++ crawler/management/commands/generate_robots_config.py
@@ -1,11 +1,19 @@
 # coding: utf-8
 from crawler.robots_config_generator import generate_robots_config
-
-__author__ = 'vladimir'
-
 from django.core.management import BaseCommand
+from optparse import make_option, OptionError
 
 
 class Command(BaseCommand):
+
+    option_list = BaseCommand.option_list + (
+        make_option('-d', '--directory',
+                    dest='directory',
+                    help='Project directory'),
+        make_option('-u', '--user',
+                    dest='user',
+                    help='Directory owner', ))
+
     def handle(self, *args, **kwargs):
-        generate_robots_config()
+        d, u = kwargs.get('directory'),  kwargs.get('user')
+        generate_robots_config(d, u)
