7daa8613bb8219b72784efb9d41f54430488c748
Denis Lyubimov <dlyubimov@aaysm.com>
2014-10-24 14:07:06 +0400

    Changed settings for player.html

--- interface/static/js/config.js
+++ interface/static/js/config.js
@@ -18,7 +18,7 @@
 
   conf.noperson_url = conf.images_url + "noperson.jpg";
 
-  conf.player_url = "/player.html";
+  conf.player_url = "/static/player.html";
 
   conf.player_cast_url = "static/playercast.html";
 
