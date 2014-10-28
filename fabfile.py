5012d3e34ba546f7f992288ffa62f3dc0c7b8281
tumani1 <itumanov@aaysm.com>
2014-10-28 14:02:51 +0300

    - правка по подсчету консолидированного рейтинга

--- fabfile.py
+++ fabfile.py
@@ -3,23 +3,17 @@
 import os
 import time
 import fabtools
-from StringIO import StringIO
 
 from string import Template
-from fabric.api import env, roles, run, settings, sudo, cd, local, require, get
+from fabric.api import env, roles, run, settings, sudo, cd, local, require, get, put
 
-# env.roledefs = {
-#     'production': ['developers@5.9.17.109'],
-#     'staging': ['developers@188.226.191.166'],
-#     'test': ['tumani1@127.0.0.1'],
-# }
 
 common_packages = [
     'python', 'build-essential', 'python-dev',
     'python-setuptools', 'python-pip'
 ]
 
-env.git = 'git@git.aaysm.com:developers/videobase.git'
+env.git_clone = 'git@git.aaysm.com:developers/videobase.git'
 
 ####################################################################
 # Environments
@@ -31,9 +25,15 @@ def localhost_env():
     env.project_name = 'videobase2'
     env.path = '/home/%(user)s/workspace/%(project_name)s' % env
     env.env = '/home/%(user)s/venv' % env
+    env.current_path = '%(path)s/current' % env
+    env.releases_path = '%(path)s/releases' % env
+    env.req_dir = 'deploy'
+    env.services = ['supervisor', 'nginx']
+
     env.pip = '%(env)s/bin/pip' % env
     env.python = '%(env)s/bin/python' % env
     env.shell = '/bin/bash -c'
+
 
 def production_env():
     "Use the actual webserver"
@@ -42,148 +42,237 @@ def production_env():
     env.project_name = 'videobase2'
     env.path = '/var/www/%(project_name)s' % env
     env.env = '/home/%(user)s/venv' % env
+    env.current_path = '%(path)s/current' % env
+    env.releases_path = '%(path)s/releases' % env
+    env.req_dir = 'deploy'
+    env.services = ['supervisor', 'nginx']
+
     env.pip = '%(env)s/bin/pip' % env
     env.python = '%(env)s/bin/python' % env
     env.shell = '/bin/bash -c'
 
-# def production_env():
-#     """Окружение для продакшена"""
-#     # env.key_filename = [os.path.join(os.environ['HOME'], '.ssh', 'git_example_org')] # Локальный путь до файла с ключами
-#     env.user = 'developers'  # На сервере будем работать из под пользователя "git"
-#     env.project_root = '/home/developers/videobase2'  # Путь до каталога проекта (на сервере)
-#     env.shell = '/bin/bash -c'  # Используем шелл отличный от умолчательного (на сервере)
-#     env.python = '/home/developers/venv/bin/python'  # Путь до python (на сервере)
-#     env.env = '/home/developers/venv'
+
+# ####################################################################
+# def deploy(git_stash=True, **kwargs):
+#     require('hosts', provided_by=[localhost_env, production_env])
+#     require('path')
 #
+#     env.release = time.strftime('%Y%m%d%H%M%S')
 #
-# def test_env():
-#     """Окружение для локальной площадки"""
-#     env.run = local
-#     env.host_string = 'localhost'
-#     env.user = 'tumani1'
-#     env.project_root = '/home/tumani1/workspace/videobase'
-#     env.shell = '/bin/bash -c'
-#     env.python = '/home/tumani1/virtualenv/videobase/bin/python'
-#     env.env = '/home/tumani1/virtualenv/videobase/'
-#     env.use_ssh_config = False
+#     # Install common packages
+#     fabtools.deb.install(globals()['common_packages'])
 #
+#     # Проверка окружения
+#     if not fabtools.python.virtualenv_exists(env.env):
+#         fabtools.python.create_virtualenv(env.env)
 #
-# def staging_env():
-#     """Окружение для тестовой площадки"""
-#     # env.key_filename = [os.path.join(os.environ['HOME'], '.ssh', 'git_example_org')] # Локальный путь до файла с ключами
-#     env.user = 'developers'
-#     env.project_root = '/home/developers/videobase2'
-#     env.shell = '/bin/bash -c'
-#     env.python = '/home/developers/venv/bin/python'
-#     env.env = '/home/developers/venv'
+#     # Проверка директории проекта
+#     init_proj = False
+#     if not fabtools.files.is_dir(env.path):
+#         init_proj = True
+#         run('/bin/mkdir {dir}'.format(dir=env.path))
+#
+#         # Clone project by git
+#         fabtools.git.clone(env.git, path=env.path)
+#
+#     with cd(env.path):
+#         # Update project
+#         if not init_proj:
+#             if git_stash:
+#                 run('git stash save "Uncommitted changes before update operation at {time}"'.format(
+#                     time=env.release
+#                 ))
+#
+#             run('git pull')
+#
+#         # Require some Debian/Ubuntu packages
+#         run('cat %(sys_file)s | xargs sudo apt-get install -y ' % {
+#             'sys_file': os.path.join(env.path, env.req_dir, 'node.txt')
+#         })
+#
+#         # Require Node packages
+#         run('cat %(sys_file)s | xargs sudo npm install ' % {
+#             'sys_file': os.path.join(env.path, env.req_dir, 'node.txt')
+#         })
+#
+#         if init_proj:
+#             # Init Postgres User
+#             if not fabtools.postgres.user_exists():
+#                 fabtools.postgres.create_user(
+#                     name='pgadmin', password='qwerty',
+#                     createdb=True, createrole=True,
+#                 )
+#
+#             # Init Postgres DB
+#             if not fabtools.postgres.database_exists('videobase'):
+#                 fabtools.postgres.create_database(
+#                     name='videobase', owner='pgadmin'
+#                 )
+#
+#         # Require a Python package
+#         with fabtools.python.virtualenv(env.env):
+#             run('{pip} install -r {filepath}'.format(
+#                 pip=env.pip,
+#                 filepath=os.path.join(env.path, 'requirements.txt')
+#             ))
+#
+#             # Сбор статики
+#             run('{python} manage.py collectstatic --dry-run --noinput'.format(
+#                 python=env.python
+#             ))
+#
+#             if init_proj:
+#                 run('{python} manage.py migrate syncdb'.format(python=env.python))
+#
+#             # # Миграции
+#             # run('{python} manage.py migrate --no-initial-data'.format(
+#             #     python=env.python
+#             # ))
+#
+#             # Restart сервисов
+#             restart_services()
 
 
-####################################################################
-def deploy(git_stash=True, **kwargs):
-    # roledefs = {
-    #     'production': ['developers@5.9.17.109'],
-    #     'staging': ['developers@188.226.191.166'],
-    #     'test': ['tumani1@127.0.0.1'],
-    # }
 
+def install_common_packages():
+    """
+    Установка основных общих системных пакетов
+    """
 
-    # if not envir in roledefs:
-    #     raise ValueError('This enviroment "{env}" does exist'.format(env=envir))
-
-    # # Init enviroment
-    # envir_func = '{env_name}_env'.format(env_name=envir)
-    # globals()[envir_func]()
-
-    require('hosts', provided_by=[localhost_env, production_env])
-    require('path')
-
-    env.release = time.strftime('%d.%m.%y %H:%M')
-
-    # Install common packages
     fabtools.deb.install(globals()['common_packages'])
+
+
+def install_all_sys_packages():
+    """
+    Установка системных пакетов
+    """
+
+    if not 'current_release' in env:
+        releases()
+
+    run('cat %(sys_file)s | xargs sudo apt-get install -y ' % {
+        'sys_file': os.path.join(env.current_release, env.req_dir, 'system.txt')
+    })
+
+
+def install_npm_packages():
+    """
+    Установка NodeJS пакетов
+    """
+
+    if not 'current_release' in env:
+        releases()
+
+    run('cat %(sys_file)s | xargs sudo npm install ' % {
+        'sys_file': os.path.join(env.current_release, env.req_dir, 'node.txt')
+    })
+
+
+def releases():
+    """
+    Список релизов, которые были сделаны
+    """
+
+    env.releases = sorted(run('ls -x %(releases_path)s' % env).split())
+    if len(env.releases) >= 1:
+        env.current_revision = env.releases[-1]
+        env.current_release = "%(releases_path)s/%(current_revision)s" % env
+    if len(env.releases) > 1:
+        env.previous_revision = env.releases[-2]
+        env.previous_release = "%(releases_path)s/%(previous_revision)s" % env
+
+
+def checkout():
+    """
+    """
+
+    from time import time
+    env.current_release = "%(releases_path)s/%(time).0f" % {'releases_path': env.releases_path, 'time': time()}
+
+    with cd(env.releases_path):
+        run("git clone -q -o deploy --depth 1 %(git_clone)s %(current_release)s" % env)
+
+
+def update_env():
+    """
+    Обновление python окружения
+    """
+
+    if not 'current_release' in env:
+        releases()
 
     # Проверка окружения
     if not fabtools.python.virtualenv_exists(env.env):
         fabtools.python.create_virtualenv(env.env)
 
-    # Проверка директории проекта
-    init_proj = False
+    with cd(env.current_release):
+        # Установка python пакетов
+        with fabtools.python.virtualenv(env.env):
+            run('{pip} install -r %(current_release)s/%(req_file)s' % env)
+
+
+def symlink():
+    """
+    Устанавливаем символические ссылки
+    """
+
+    if not 'current_release' in env:
+        releases()
+
+    run('ln -nfs %(current_release)s %(current_path)s' % env)
+
+
+def restart_services():
+    """
+    Перезапуск всех сервисов проекта
+    """
+
+    for service in env.get('services', []):
+        fabtools.service.restart(service)
+
+
+def setup():
     if not fabtools.files.is_dir(env.path):
-        init_proj = True
         run('/bin/mkdir {dir}'.format(dir=env.path))
 
-        # Clone project by git
-        fabtools.git.clone(env.git, path=env.path)
-
-    with cd(env.path):
-        # Update project
-        if not init_proj:
-            if git_stash:
-                run('git stash save "Uncommitted changes before update operation at {time}"'.format(
-                    time=env.release
-                ))
-
-            run('git pull')
-
-        # Require some Debian/Ubuntu packages
-        fd = StringIO()
-        get(os.path.join(env.path, "system.txt"), fd)
-        content = fd.getvalue()
-        fd.close()
-
-        sys_packages = content.split("\n")
-        if len(sys_packages):
-            fabtools.deb.install(sys_packages)
-
-        # Require Node packages
-        fd = StringIO()
-        get(os.path.join(env.path, "node.txt"), fd)
-        content = fd.getvalue()
-        fd.close()
-
-        sys_packages = content.split("\n")
-        # if len(sys_packages):
-        #     fabtools.nodejs.install_package(' '.join(sys_packages))
-
-        if init_proj:
-            # Init Postgres User
-            if not fabtools.postgres.user_exists():
-                fabtools.postgres.create_user(
-                    name='pgadmin', password='qwerty',
-                    createdb=True, createrole=True,
-                )
-
-            # Init Postgres DB
-            if not fabtools.postgres.database_exists('videobase'):
-                fabtools.postgres.create_database(
-                    name='videobase', owner='pgadmin'
-                )
-
-        # Require a Python package
-        with fabtools.python.virtualenv(env.env):
-            run('{pip} install -r {filepath}'.format(
-                pip=env.pip,
-                filepath=os.path.join(env.path, 'requirements.txt')
-            ))
-
-            # Сбор статики
-            run('{python} manage.py collectstatic --dry-run --noinput'.format(
-                python=env.python
-            ))
-
-            if init_proj:
-                run('{python} manage.py migrate syncdb'.format(python=env.python))
-
-            # # Миграции
-            # run('{python} manage.py migrate --no-initial-data'.format(
-            #     python=env.python
-            # ))
-
-            # Restart сервисов
-            restart_services()
+    run("mkdir -p %(path)s/{releases,current}" % env)
 
 
-#
+def deploy():
+    require('hosts', provided_by=[localhost_env, production_env])
+    require('path')
+
+    setup()
+    checkout()
+    update_env()
+    symlink()
+    restart_services()
+
+
+def rollback():
+    pass
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 # def init_db():
 #     """
 #     Создает базу данных и пользователя для основного сайта
@@ -235,18 +324,8 @@ def deploy(git_stash=True, **kwargs):
 #             else:
 #                 sudo('git clone git@git.aaysm.com:developers/videobase.git test_base')
 #                 sudo("cd test_base/configs/ && cp db.ini.example db.ini && sed -i 's/videobase/test_base/g' db.ini")
-
-
-def restart_services():
-    """
-    Перезапуск всех процессов
-    """
-
-    list_services = ['supervisor', 'nginx']
-    for service in list_services:
-        fabtools.service.restart(service)
-
-
+#
+#
 # def delete_test_db():
 #     """
 #     Удалить тестовую базу данных
