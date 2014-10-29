a01a9ddfb2d868db5ced8c28d4ff7c5f13529b20
Vladimir Mikryukov <vmikrukov@aaysm.com>
2014-10-29 18:00:25 +0400

    Added logic for useless words checking in trailer title

--- data/film_facts/trailer_title_check.py
+++ data/film_facts/trailer_title_check.py
@@ -14,13 +14,18 @@ def is_correct_trailer_title(title, film):
         check_block = False
         check_fname = False
         check_fyear = False
-        for phrase in trailers_ru_mask:
+        check_has_many_useless_words = False
+        ru_match = u''
+        en_match = u''
+        for index, phrase in trailers_ru_mask:
             if title.find(phrase) != -1:
                 check_ru = True
+                ru_match = trailers_ru_mask[index]
 
-        for phrase in trailers_en_mask:
+        for index, phrase in trailers_en_mask:
             if title.find(phrase) != -1:
                 check_en = True
+                en_match = trailers_en_mask[index]
 
         for phrase in trailers_block:
             if title.find(phrase) != -1:
@@ -32,7 +37,15 @@ def is_correct_trailer_title(title, film):
 
         if tr_t_for_comparison.find(str(film_year)) != -1:
             check_fyear = True
-        if (check_ru or check_en) and check_fname and check_fyear and not check_block:
+
+        if trailers_ru_mask or trailers_en_mask and not check_block: #проверяем много ли лишних слов помимо шаблонных
+            cutted_title = title.Replace(en_match, '').Replace(ru_match, '').Replace(film_name, '').Replace(str(film_year), '').split(' ')
+            if len(cutted_title)>3:
+                check_has_many_useless_words = True
+            else:
+                check_has_many_useless_words = False
+        print "ru:",check_ru, "en:",en_match, "name:",check_fname, "year:",check_fyear, "block:", check_block, "useless_words:", check_has_many_useless_words
+        if (check_ru or check_en) and check_fname and check_fyear and not check_block and not check_has_many_useless_words:
             return True
         else:
             return False
