4c484172b56ff45763423d5eb499eefbdf1b5d77
Dmitriy Krasnov <dkrasnov@aaysm.com>
2014-10-29 17:53:39 +0300

    расширена генерация конфигов, убран список роботов из внешнего классаб расширена команда генерации

--- crawler/robots_config_generator.py
+++ crawler/robots_config_generator.py
@@ -1,61 +1,65 @@
 # coding: utf-8
 from sys import executable
+from os.path import abspath
 from crawler.robots_type_checker import RobotsTypeChecker
 
 
-def generate_robots_config(directory='.', user='www-data', file_name='robots_config.conf'):
-    d, u, fn = directory, user, file_name  # сокращение
-    queue_command = lambda robot_name: executable + " manage.py celery worker -Q {}".format(robot_name)
-    main_queue_command = lambda: executable + " manage.py celery worker"
+def generate_robots_config(directory='.', user='developers', file_name='robots_config.conf', hostname='vsevi.ru'):
+    d = abspath(directory)
+    u, fn, hn = user, file_name, hostname  # сокращение
+    worker_command = lambda robot_name: executable + " manage.py celery worker -c 1 -Q {0} -n {0}.%h ".format(robot_name)
+    main_worker_command = executable + " manage.py celery worker -c 1"
+    beat_worker_command = executable + " manage.py celery beat --pidfile=/tmp/celerybeat.pid"
 
-    template = '''
-    [group:{}]
-    programs={}'''
-    generate_group = lambda group_name, process_list: template.format(group_name, process_list)
 
-    template = '''
-    [program:{}]
-    command={}
-    process_name=%(program_name)s ;
-    directory={} ;
-    umask=022 ;
-    startretries=1 ;
-    user={} ;
-    redirect_stderr=true'''
-    generate_program = lambda program_name, command, dir_, u: template.format(program_name, command, dir_, u)
+    template_gr = '''
+[group:{}]
+programs={}'''
+    generate_group = lambda group_name, processes: template_gr.format(group_name, processes)
 
-    rtc = RobotsTypeChecker()  # отсюда берутся списки роботов
+    template_pr = '''
+[program:{}]
+command={}
+process_name=%(program_name)s ;
+directory={} ;
+umask=022 ;
+startretries=1 ;
+user={} ;
+redirect_stderr=true'''
+    generate_program = lambda program_name, command, dir_, u: template_pr.format(program_name, command, dir_, u)
+
+    rtc = RobotsTypeChecker()
     lr_list = rtc.get_locrobots_list()
     dr_list = rtc.get_datarobots_list()
-    sr_list = rtc.get_support_robots()
-    cr_list = rtc.get_casts_robot()
+    cr_list = ['cast_sportbox_robot', 'cast_liverussia_robot', 'cast_championat_robot', 'cast_khl_robot', 'cast_ntv_plus_robot']
+    adr_list = ['robot_beat', 'robot_statistics_email', 'robot_notification_email', 'robot_newsletter_email',
+                'robot_avatar', 'location_saver', 'thor']
 
     with open(fn, 'w') as file:
         for robot in lr_list:
-            command = queue_command(robot)
+            command = worker_command(robot)
             file.write(generate_program(robot, command, d, u))
             file.write("\n")
 
         for robot in dr_list:
-            command = queue_command(robot)
+            command = worker_command(robot)
             file.write(generate_program(robot, command, d, u))
             file.write("\n")
 
         for robot in cr_list:
-            command = queue_command(robot)
+            command = worker_command(robot)
             file.write(generate_program(robot, command, d, u))
             file.write("\n")
 
-        command = queue_command('location_saver')
-        file.write(generate_program('location_saver', command, d, u))
+        for robot in adr_list:
+            command = worker_command(robot)
+            file.write(generate_program(robot, command, d, u))
+            file.write("\n")
+
+        file.write(generate_program('main_worker', main_worker_command, d, u))
         file.write("\n")
 
-        command = queue_command('thor')
-        file.write(generate_program('thor', command, d, u))
-        file.write("\n")
-
-        command = main_queue_command()
-        file.write(generate_program('main_worker', command, d, u))
+        file.write(generate_program('beat_worker', beat_worker_command, d, u))
         file.write("\n")
 
         file.write(generate_group('locrobots', ','.join(lr_list)))
@@ -64,10 +68,10 @@ def generate_robots_config(directory='.', user='www-data', file_name='robots_con
         file.write(generate_group('datarobots', ','.join(dr_list)))
         file.write("\n")
 
-        file.write(generate_group('castsrobots', ','.join(cr_list)))
+        file.write(generate_group('castrobots', ','.join(cr_list)))
         file.write("\n")
 
-        file.write(generate_group('supportrobots', ','.join(sr_list)))
+        file.write(generate_group('additionalrobots', ','.join(['main_worker']+adr_list)))
         file.write("\n")
 
     return fn
