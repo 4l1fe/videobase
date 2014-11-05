6a91a8649c3285331a6e27dc2b0a138d65eec9a5
Eugene <eshangin@aaysm.com>
2014-11-05 14:22:40 +0400

    Доделал робота zoomby

--- crawler/management/commands/amediateka_robot_start.py
+++ crawler/management/commands/amediateka_robot_start.py
@@ -4,7 +4,7 @@ from crawler.locrobots.individual_tasks import process_individual_film_on_site
 
 class Command(NoArgsCommand):
     def handle_noargs(self, **options):
-        process_individual_film_on_site.run('zoomby_ru', 7597)
+        process_individual_film_on_site.run('zoomby_ru', 35421)
 
 
 
