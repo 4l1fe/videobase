b018f8ecf058aa1b5d851d66e2532ed0eb88d73d
Eugene <eshangin@aaysm.com>
2014-11-07 14:34:09 +0400

    Переписал метод get_content

--- crawler/utils/locations_utils.py
+++ crawler/utils/locations_utils.py
@@ -44,7 +44,7 @@ def get_content(film, kwargs):
     object will be created with these defaults
     '''
     # Getting all content with this film
-    contents = Contents.objects.filter(film=film)
+    contents = Contents.get_content_by_film(film)
 
     if 'number' in kwargs:
         season_num = kwargs['number']
@@ -61,12 +61,6 @@ def get_content(film, kwargs):
     else:
         description = None
 
-    if 'content_type' in kwargs:
-        content_type = kwargs['content_type']
-    else:
-        content_type = None
-
-
     if len(contents) == 0:
         #If there is no such content just creating one with meaningful defaults
 
@@ -82,68 +76,35 @@ def get_content(film, kwargs):
 
             return content
         else:
-            if content_type == APP_LOCATION_TYPE_ADDITIONAL_MATERIAL_SEASON:
-                season = Seasons(film=film, number=season_num)
-                season.save()
-                content = Contents(film=film, name=film.name, name_orig=film.name_orig,
-                               description=description,
-                               release_date=film.release_date,
-                               viewer_cnt=0,
-                               viewer_lastweek_cnt=0,
-                               viewer_lastmonth_cnt=0,
-                               season=season)
-                content.save()
-                return content
-            else:
-                content = Contents.objects.filter(film=film, season=season_num)
-                if len(content) == 0:
-                    season = Seasons(film=film, number=season_num)
-                    season.save()
-                    content = Contents(film=film, name=film.name, name_orig=film.name_orig,
-                                   description=description,
-                                   release_date=film.release_date,
-                                   viewer_cnt=0,
-                                   viewer_lastweek_cnt=0,
-                                   viewer_lastmonth_cnt=0,
-                                   season=season)
-                    content.save()
-                    return content
-                else:
-                    return Contents.objects.filter(film=film, season=season_num)
+            season = Seasons(film=film, number=season_num, release_date=release_date)
+            season.save()
+            content = Contents(film=film, name=film.name, name_orig=film.name_orig,
+                           description=description,
+                           release_date=film.release_date,
+                           viewer_cnt=0,
+                           viewer_lastweek_cnt=0,
+                           viewer_lastmonth_cnt=0,
+                           season=season)
+            content.save()
+            return content
 
     else:
         print season_num
         #According to contract we agreed if there are no seasons there should be return 0, buy some code may return None
         if (season_num is None) or (season_num == 0):
-            content = contents[0]
+            content = Contents.get_content_by_film_and_number(film, 0)[0]
         else:
-            content = next((c for c in contents if c.season.number == season_num), None)
-
-            if content is None:
-
-                precontent = contents[0]
-                if release_date is None:
-                    print u"Assigned release date for new content based on release date for the film %d" %   film.pk
-                    release_date = film.release_date
-
-                if 'series_cnt' in kwargs:
-                    series_cnt = kwargs['series_cnt']
-                else:
-                    print u"Assigned new series count in season to number of series in previous season for season %d for film %d" % ( season_num, film.pk)
-                    series_cnt = precontent.season.series_cnt
-
-                season = Seasons(film=film, release_date=release_date,
-                                 series_cnt=series_cnt, description=description,
-                                 number=season_num)
-                content = Contents(film=film, name=precontent.name,
-                                   name_orig=precontent.name_orig,
-                                   description=description, number=season_num,
-                                   release_date=release_date,
-                                   viewer_cnt=kwargs['viewer_cnt'],
-                                   season=season,
-                                   viewer_lastweek_cnt=kwargs['viewer_cnt'],
-                                   viewer_lastmonth_cnt=kwargs['viewer_cnt'])
+            content = Contents.get_content_by_film_and_number(film, season_num)
+            if len(content) == 0:
+                new_season = Seasons(film=film, number=season_num)
+                new_season.save()
+                content = Contents.get_content_by_film(film)
+                content.season = new_season
                 content.save()
+                return content
+            else:
+                return content[0]
+
         return content
 
 
