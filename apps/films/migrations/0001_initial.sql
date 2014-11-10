39cc6354d6203355d60ac24b871446a3c7cf7d20
tumani1 <itumanov@aaysm.com>
2014-11-10 11:04:16 +0300

    - удалил файл с sql и добавил  миграцию

--- apps/films/migrations/0001_initial.sql
+++
@@ -1,4 +0,0 @@
-create extension if not exists unaccent;
-
-create text search configuration public.film_names (parser='default');
-alter text search configuration film_names alter mapping for asciiword, asciihword, hword_asciipart, hword_numpart, word, hword, numword, hword_part with unaccent, simple;
