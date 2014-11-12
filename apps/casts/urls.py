fb3148abbd321a28f5de82e7130de4d074aaa6c5
Ivan <ikrivosheev@aaysm.com>
2014-11-12 16:59:16 +0300

    Исправлено API для трансляций.

--- apps/casts/urls.py
+++ apps/casts/urls.py
@@ -13,7 +13,7 @@ v1_api_patterns = patterns('',
     url(r'^v1/casts/(?P<cast_id>\d+)/extras', CastsExtraView.as_view(), name='cast_extras_view'),
     url(r'^v1/casts/(?P<cast_id>\d+)/rating$', CastsRatingView.as_view(), name='cast_rating_view'),
     url(r'^v1/casts/(?P<cast_id>\d+)/subscribe$', CastsSubscribeView.as_view(), name='cast_subscribe_view'),
-    url(r'^v1/castschats/(?P<castchat_id>\d+)/msgs$', CastsChatsMsgsView.as_view(), name='castchat_msgs'),
+    url(r'^v1/castschats/(?P<cast_id>\d+)/msgs$', CastsChatsMsgsView.as_view(), name='castchat_msgs'),
     url(r'^v1/castschats/(?P<cast_id>\d+)/send$', CastsChatSendView.as_view(), name='castchat_send_view'),
     url(r'^v1/castschats/(?P<castchat_id>\d+)/users$', CastsChatsUsersView.as_view(), name='castchat_users'),
 )
