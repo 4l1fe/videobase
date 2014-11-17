76bfb56d2a8b6d970df0faef5b76a786cfb70d1b
tumani1 <itumanov@aaysm.com>
2014-11-17 19:05:54 +0300

    -лишняя модель

--- apps/casts/tasks.py
+++ apps/casts/tasks.py
@@ -7,7 +7,7 @@ from django.contrib.auth.models import User
 from videobase.celery import app
 
 from apps.casts.api.serializers import vbCast
-from apps.casts.models import Casts, CastsLocations
+from apps.casts.models import Casts
 from apps.casts.constants import APP_CASTS_MAIL_SUBJECT, APP_CASTS_MAIL_TEMPLATE
 
 from apps.users.tasks import send_template_mail
