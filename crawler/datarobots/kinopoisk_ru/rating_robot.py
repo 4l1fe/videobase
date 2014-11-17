e657c1ca0ef9f690014e751fa1dee0651958a4ee
Vladimir Mikryukov <vmikrukov@aaysm.com>
2014-11-17 10:58:30 +0400

    Fixed kinopoisk ratings robot

--- crawler/datarobots/kinopoisk_ru/rating_robot.py
+++ crawler/datarobots/kinopoisk_ru/rating_robot.py
@@ -3,20 +3,32 @@
 import requests
 from bs4 import BeautifulSoup
 
+
 def get_ratings(kinopoisk_id):
 
     r = requests.get('http://kinopoisk.ru/rating/{}.xml'.format(kinopoisk_id))
     soup = BeautifulSoup(r.content,'xml')
-
-    return {'kp':
-            {'votes' : int(soup.rating.kp_rating.attrs['num_vote']),
-             'rating' : float(soup.rating.kp_rating.text)
-         },
-            'imdb':
-            {'votes' : int(soup.rating.imdb_rating.attrs['num_vote']),
-             'rating' : float(soup.rating.imdb_rating.text)
-         }
+    ratings = {
+        'kp':
+            {
+                'votes': 0,
+                'rating': 0.0
+            },
+        'imdb':
+            {
+                'votes': 0,
+                'rating': 0.0
+            }
     }
+    if soup.rating.kp_rating:
+        ratings['kp']['votes'] = int(soup.rating.kp_rating.attrs['num_vote'])
+        ratings['kp']['rating'] = float(soup.rating.kp_rating.text)
+
+    if soup.rating.imdb_rating:
+        ratings['imdb']['votes'] = int(soup.rating.imdb_rating.attrs['num_vote'])
+        ratings['imdb']['rating'] = float(soup.rating.imdb_rating.text)
+
+    return ratings
     
 
 
