d167f3682dfde976a4bb1bf0e755fc945d87ce97
Vladimir Mikryukov <vmikrukov@aaysm.com>
2014-11-17 11:18:13 +0400

    Additional fix kinopoisk ratings

--- crawler/datarobots/kinopoisk_ru/rating_robot.py
+++ crawler/datarobots/kinopoisk_ru/rating_robot.py
@@ -9,24 +9,26 @@ def get_ratings(kinopoisk_id):
     r = requests.get('http://kinopoisk.ru/rating/{}.xml'.format(kinopoisk_id))
     soup = BeautifulSoup(r.content,'xml')
     ratings = {
-        'kp':
-            {
-                'votes': 0,
-                'rating': 0.0
-            },
-        'imdb':
-            {
-                'votes': 0,
-                'rating': 0.0
-            }
+        'kp': None,
+        'imdb': None
     }
     if soup.rating.kp_rating:
-        ratings['kp']['votes'] = int(soup.rating.kp_rating.attrs['num_vote'])
-        ratings['kp']['rating'] = float(soup.rating.kp_rating.text)
+        kp_dict = {
+                'votes': 0,
+                'rating': 0.0
+        }
+        kp_dict['votes'] = int(soup.rating.kp_rating.attrs['num_vote'])
+        kp_dict['rating'] = float(soup.rating.kp_rating.text)
+        ratings['kp'] = kp_dict
 
     if soup.rating.imdb_rating:
-        ratings['imdb']['votes'] = int(soup.rating.imdb_rating.attrs['num_vote'])
-        ratings['imdb']['rating'] = float(soup.rating.imdb_rating.text)
+        imdb_dict = {
+                'votes': 0,
+                'rating': 0.0
+        }
+        imdb_dict['votes'] = int(soup.rating.imdb_rating.attrs['num_vote'])
+        imdb_dict['rating'] = float(soup.rating.imdb_rating.text)
+        ratings['imdb'] = imdb_dict
 
     return ratings
     
