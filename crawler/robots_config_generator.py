a99e0e049fa3dc7e90590c7902b4ae507fc76af5
Dmitriy Krasnov <dkrasnov@aaysm.com>
2014-11-28 14:38:30 +0300

    подправлен

--- crawler/robots_config_generator.py
+++ crawler/robots_config_generator.py
@@ -1,24 +1,17 @@
 # coding: utf-8
 from sys import executable
 from os.path import abspath
-from videobase.settings import DATAROBOTS_SCHEDULE, LOCATIONROBOTS_SCHEDULE, CASTROBOT_SCHEDULE, CELERYBEAT_SCHEDULE, \
-    CAST_QUEUE, MAIL_QUEUE, NOTIFY_QUEUE, DATA_QUEUE, LOCATION_QUEUE_P, LOCATION_QUEUE_S
+from videobase.settings import CAST_QUEUE, MAIL_QUEUE, NOTIFY_QUEUE, DATA_QUEUE, LOCATION_QUEUE_P, LOCATION_QUEUE_S
 
 
 def generate_robots_config(directory='.', user='developers', file_name='robots_config.conf'):
     d = abspath(directory)
     u, fn = user, file_name  # сокращение
-    worker_command = lambda queues, worker_name: executable + " manage.py celery worker -c 1 -Q {0} -n {1}.%%h ".format(queues, worker_name)
-    main_command = executable + " manage.py celery worker -c 1"
+    worker_command = lambda queue, worker_name: executable + " manage.py celery worker -c 1 -Q {0} -n {1}.%%h ".format(queue, worker_name)
+    main_command = executable + " manage.py celery worker -c 1 -n main.%%h"
     beat_command = executable + " manage.py celery beat --pidfile=/tmp/celerybeat.pid"
 
-    template_gr = '''
-[group:{}]
-programs={}'''
-    generate_group = lambda group_name, processes: template_gr.format(group_name, processes)
-
-    template_pr = '''
-[program:{}]
+    template_pr = '''[program:{}]
 command={}
 process_name=%(program_name)s ;
 directory={} ;
@@ -29,46 +22,30 @@ redirect_stderr=true ;
 stdout_logfile=/var/log/%(program_name)s.log'''
     generate_program = lambda program_name, command, dir_, u: template_pr.format(program_name, command, dir_, u)
 
-    lt_list = []
-    dt_list = []
-    ct_list = []
-    adnt_list = []
-
-    for key, value in LOCATIONROBOTS_SCHEDULE.iteritems():
-        lt_list += [key]
-
-    for key, value in DATAROBOTS_SCHEDULE.iteritems():
-        dt_list += [key]
-
-    for key, value in CASTROBOT_SCHEDULE.iteritems():
-        ct_list += [key]
-
-    for key, value in CELERYBEAT_SCHEDULE.iteritems():
-        ct_list += [key]
-
     with open(fn, 'w') as file:
-
+        
         file.write(generate_program('main', main_command, d, u))
-        file.write("\n")
+        file.write("\n\n")
 
         file.write(generate_program('beat', beat_command, d, u))
-        file.write("\n")
+        file.write("\n\n")
 
-        cast_queue = [CAST_QUEUE]
-        mail_notify_queue = [MAIL_QUEUE, NOTIFY_QUEUE]
-        data_queue = [DATA_QUEUE]
-        location_queue = [LOCATION_QUEUE_S, LOCATION_QUEUE_P]
+        file.write(generate_program(CAST_QUEUE, worker_command(CAST_QUEUE, CAST_QUEUE+'_worker'), d, u))
+        file.write("\n\n")
 
-        file.write(generate_program(CAST_QUEUE, worker_command(','.join(cast_queue), CAST_QUEUE+'_worker'), d, u))
-        file.write("\n")
+        file.write(generate_program(MAIL_QUEUE, worker_command(MAIL_QUEUE, MAIL_QUEUE+'_worker'), d, u))
+        file.write("\n\n")
 
-        file.write(generate_program(MAIL_QUEUE+'_'+NOTIFY_QUEUE, worker_command(','.join(mail_notify_queue), MAIL_QUEUE+'_'+NOTIFY_QUEUE+'_worker'), d, u))
-        file.write("\n")
+        file.write(generate_program(NOTIFY_QUEUE, worker_command(NOTIFY_QUEUE, NOTIFY_QUEUE+'_worker'), d, u))
+        file.write("\n\n")
 
-        file.write(generate_program(DATA_QUEUE, worker_command(','.join(data_queue), DATA_QUEUE + '_worker'), d, u))
-        file.write("\n")
+        file.write(generate_program(DATA_QUEUE, worker_command(DATA_QUEUE, DATA_QUEUE + '_worker'), d, u))
+        file.write("\n\n")
 
-        file.write(generate_program('locations', worker_command(','.join(location_queue), 'locations_worker'), d, u))
-        file.write("\n")
+        file.write(generate_program(LOCATION_QUEUE_S, worker_command(LOCATION_QUEUE_S, LOCATION_QUEUE_S+'_worker'), d, u))
+        file.write("\n\n")
+
+        file.write(generate_program(LOCATION_QUEUE_P, worker_command(LOCATION_QUEUE_P, LOCATION_QUEUE_P+'_worker'), d, u))
+        file.write("\n\n")
 
     return fn
